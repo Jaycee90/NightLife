9814a727d15f6eb53dff499f6feae029179b4ff8
Nilu Sah <zys5@txstate.edu>
2023-09-28 17:47:38 -0500

    Added SpecialEvent (Practice)

--- VibeTrack/src/App.js
+++ VibeTrack/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Routes/Home';
 import RouteOne from './Routes/RouteOne';
+import SpecialEvent from './Routes/Special';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/routeone' element={<RouteOne />} />
+        <Route path='/specialevent' element={<SpecialEvent/>} />
       </Routes>
     </Router>
   );
