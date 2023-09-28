67d9b0d28c61c2e355258d72090ee941c7a4e619
Isaiah Gage <frb32@txstate.edu>
2023-09-27 21:03:56 -0500

    Added Routes

--- vibetest/src/App.js
+++ vibetest/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Routes/Home';
+import RouteOne from './Routes/RouteOne';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Hey
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/routeone' element={<RouteOne />} />
+      </Routes>
+    </Router>
   );
 }
 
