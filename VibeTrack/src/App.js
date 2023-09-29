39fde2b5a9d3c29871b105372a8425a07624545c
Jayce Turambe <jnn56@txstate.edu>
2023-09-29 14:11:48 -0500

    Added map to listing.js
    Success!

--- VibeTrack/src/App.js
+++ VibeTrack/src/App.js
@@ -8,10 +8,7 @@ import Calendar from './Routes/Calendar';
 import Listing from './Routes/Listing';
 import Login from './Routes/Login';
 
-import "leaflet/dist/leaflet.css";
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+
 
 function App() {
   return (
@@ -26,25 +23,6 @@ function App() {
         <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
-  );
-
-  return(
-    <div style={{ display: "flex" }}>
-      <MapContainer
-        style={{
-          height: "100vh",
-          width: "100%",
-        }}
-        center={[31.432026740690574, 120.8439179532812]}
-        zoom={8}
-      >
-        {/* Add Google Maps tile URL */}
-        <TileLayer
-          attribution="Google Maps"
-          url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
-        />
-      </MapContainer>
-    </div>
   );
 
 }
--- VibeTrack/src/Routes/Listing.js
+++ VibeTrack/src/Routes/Listing.js
@@ -1,13 +1,29 @@
 import React from "react";
+import "leaflet/dist/leaflet.css";
 
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 function Listing() {
-    return (
-        <div>
-            <h1>This is the Vibetrack Listing.</h1>
-            <h2>Find all the clubs near you here!! enjoy your night</h2>
-  
-        </div>
-    );
+    
+  return(
+    <div style={{ display: "flex" }}>
+      <MapContainer
+        style={{
+          height: "100vh",
+          width: "100%",
+        }}
+        center={[31.432026740690574, 120.8439179532812]}
+        zoom={8}
+      >
+        {/* Add Google Maps tile URL */}
+        <TileLayer
+          attribution="Google Maps"
+          url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+        />
+      </MapContainer>
+    </div>
+  );
+
 };
 
 export default Listing;
