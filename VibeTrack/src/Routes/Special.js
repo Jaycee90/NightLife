0d74fcf505f0763fc7cc5bd4fc6c758556deabd8
Nilu Sah <zys5@txstate.edu>
2023-09-29 16:25:25 -0500

    Startpoint: Calendar for Special Event

--- VibeTrack/src/Routes/Special.js
+++ VibeTrack/src/Routes/Special.js
@@ -3,7 +3,7 @@ import React from "react";
 function SpecialEvent() {
     return (
         <div>
-            <h1>This is the special event!</h1>
+            <h1>This is the special event! Nilu</h1>
         </div>
     );
 };
