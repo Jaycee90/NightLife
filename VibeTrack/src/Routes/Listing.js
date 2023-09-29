0e8dcad2b1385b7652af7f24a9f115aaa9988fa2
Jayce Turambe <jnn56@txstate.edu>
2023-09-29 15:08:17 -0500

    Add marker to map
    Success

--- VibeTrack/src/Routes/Listing.js
+++ VibeTrack/src/Routes/Listing.js
@@ -2,9 +2,15 @@ import React from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 function Listing() {
     const locationCoord = [29.8833, -97.9414];
+    const icon = L.icon({ iconUrl: "https://i.imgur.com/yyb78tO.png" });
+
+    // some other code
+
+    
     
   return(
     <div style={{ display: "flex" }}>
@@ -21,11 +27,12 @@ function Listing() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[29.8833, -97.9414]}>
+        <Marker position={[29.8833, -97.9414]} icon={icon}>
             <Popup>
             Your Location <br /> Coordinates: {locationCoord[0]}, {locationCoord[1]}
             </Popup>
         </Marker>
+    
       </MapContainer>
     </div>
   );
