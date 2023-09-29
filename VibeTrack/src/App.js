44087cfeb00b182b0679c1c4d4df767b630811ff
Jayce Turambe <jnn56@txstate.edu>
2023-09-29 14:04:18 -0500

    stopping point: unable to render map

--- VibeTrack/src/App.js
+++ VibeTrack/src/App.js
@@ -7,6 +7,12 @@ import Search from './Routes/Search';
 import Calendar from './Routes/Calendar';
 import Listing from './Routes/Listing';
 import Login from './Routes/Login';
+
+import "leaflet/dist/leaflet.css";
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function App() {
   return (
     <Router>
@@ -21,6 +27,26 @@ function App() {
       </Routes>
     </Router>
   );
+
+  return(
+    <div style={{ display: "flex" }}>
+      <MapContainer
+        style={{
+          height: "100vh",
+          width: "100%",
+        }}
+        center={[31.432026740690574, 120.8439179532812]}
+        zoom={8}
+      >
+        {/* Add Google Maps tile URL */}
+        <TileLayer
+          attribution="Google Maps"
+          url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+        />
+      </MapContainer>
+    </div>
+  );
+
 }
 
 export default App;
--- VibeTrack/src/Routes/Listing.js
+++ VibeTrack/src/Routes/Listing.js
@@ -1,27 +1,11 @@
 import React from "react";
-import {
-    MapContainer,
-    TileLayer,
-    Marker,
-    Popup,
-  } from 'https://cdn.esm.sh/react-leaflet'
 
 function Listing() {
     return (
         <div>
             <h1>This is the Vibetrack Listing.</h1>
             <h2>Find all the clubs near you here!! enjoy your night</h2>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+  
         </div>
     );
 };
