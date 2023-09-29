acabbb9277edeff80d3dcb89bfb6cbd10994525e
Jayce Turambe <jnn56@txstate.edu>
2023-09-29 12:52:47 -0500

    Start pulling: Google map API

--- VibeTrack/src/Routes/Listing.js
+++ VibeTrack/src/Routes/Listing.js
@@ -3,7 +3,8 @@ import React from "react";
 function Listing() {
     return (
         <div>
-            <h1>This is the Vibetrack Listing yayy!!!</h1>
+            <h1>This is the Vibetrack Listing.</h1>
+            <h2>Find all the clubs near you here!! enjoy your night</h2>
         </div>
     );
 };
