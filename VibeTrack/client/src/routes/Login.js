f95bdf5d3f6a6a26087dac5c42ad750004cb2aeb
Michelle Nguyen <rnb90@txstate.edu>
2023-10-06 21:19:28 -0500

    Added routes back
    (So app.js would work properly, rename for formatting uniformity with css and components)

---
+++ VibeTrack/client/src/routes/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Discover.css';
+
+function Login() {
+    return (
+        <div>
+        <h2 style={{'color':'#000000'}}>Hello World I am the king of the world</h2>
+        </div>
+    );
+};
+
+export default Login;
