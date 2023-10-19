9bfb6ca748dbd0b760359155cec7487afaf623be
Jayce Turambe <jnn56@txstate.edu>
2023-10-19 17:14:10 -0500

    Stoppoint: Testing with location retrieving

--- VibeTrack/client/src/App.js
+++ VibeTrack/client/src/App.js
@@ -13,13 +13,15 @@ import Home from './routes/Home';
 import Discover from './routes/Discover';
 import Data from './routes/Data'; 
 import SpecialEvent from './routes/Special';
-import Search from './routes/Search';
+import Search from './routes/Search'; 
+
 import Calendar from './routes/Calendar';
 import Template from './routes/Template';
 import Login from './routes/Login';
 import Safety from './routes/Safety';
 
 import Test from './routes/Test';
+import SearchLink from "./components/link_search"; // Import the Search component
 
 const App = () => {
   return (
@@ -32,7 +34,8 @@ const App = () => {
         <Route path='/specialevent' element={<SpecialEvent/>} />
         <Route path='/calendar' element={<Calendar/>} />
         <Route path='/template' element={<Template/>} />
-        <Route path='/search' element={<Search/>} />
+        <Route path='/search' element={<Search />} /> {/* Use the Search component here */}
+        <Route path='/discover' element={<SearchLink />} />
         <Route path='/login' element={<Login />} />
         <Route path='/safety' element={<Safety />} />
 
--- VibeTrack/client/src/routes/Search.js
+++ VibeTrack/client/src/routes/Search.js
@@ -1,32 +1,52 @@
+// Import React and useState hook from the 'react' library
 import React, { useState } from "react";
+
+// Import the CSS styles for the component
 import '../css/Discover.css';
 
+// Define a functional component called 'Search'
 function Search() {
+    // Define a state variable 'locationResult' and a function to update it, 'setLocationResult'
     const [locationResult, setLocationResult] = useState('');
 
+    // Define a function to retrieve the user's geolocation
     const getUserLocation = () => {
+        // Check if the browser supports the Geolocation API
         if (navigator.geolocation) {
+            // If supported, use 'getCurrentPosition' to get the user's location
             navigator.geolocation.getCurrentPosition(
+                // Success callback function, receiving the 'position' object
                 function (position) {
+                    // Extract the latitude and longitude from 'position'
                     const latitude = position.coords.latitude;
                     const longitude = position.coords.longitude;
+                    
+                    // Update the 'locationResult' state with the user's coordinates
                     setLocationResult(`Latitude: ${latitude}, Longitude: ${longitude}`);
                 },
+                // Error callback function, receiving an 'error' object
                 function (error) {
+                    // Update the 'locationResult' state with an error message
                     setLocationResult(`Error: ${error.message}`);
                 }
             );
         } else {
+            // If Geolocation API is not supported, update 'locationResult' with an error message
             setLocationResult('Geolocation is not supported by your browser.');
         }
     };
 
+    // Render the component's JSX content
     return (
         <div>
+            {/* Create a button that triggers the 'getUserLocation' function when clicked */}
             <button onClick={getUserLocation}>Get My Location</button>
+            
+            {/* Display the 'locationResult' state, which will show the geolocation information or error message */}
             <p id="locationResult">{locationResult}</p>
         </div>
     );
 }
 
+// Export the 'Search' component to make it available for use in other parts of your application
 export default Search;
