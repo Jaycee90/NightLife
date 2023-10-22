aa7648578f05e1ddf03f3cd9f1e31c0361ea49e2
Michelle Nguyen <rnb90@txstate.edu>
2023-10-22 14:49:57 -0500

    Note: Can signup from site

--- VibeTrack/client/src/pages/Home.page.js
+++ VibeTrack/client/src/pages/Home.page.js
@@ -5,13 +5,9 @@ import { UserContext } from '../contexts/user.context';
 export default function Home() {
  const { logOutUser } = useContext(UserContext);
  
- // This function is called when the user clicks the "Logout" button.
  const logOut = async () => {
    try {
-     // Calling the logOutUser function from the user context.
      const loggedOut = await logOutUser();
-     // Now we will refresh the page, and the user will be logged out and
-     // redirected to the login page because of the <PrivateRoute /> component.
      if (loggedOut) {
        window.location.reload(true);
      }
