4ea2b80670fd8da0deab5b361984340a37c9befd
Michelle Nguyen <rnb90@txstate.edu>
2023-10-22 17:00:58 -0500

    Patched realm components to project

--- VibeTrack/client/src/App.js
+++ VibeTrack/client/src/App.js
@@ -5,16 +5,60 @@ import Login from "./realm/Login";
 import PrivateRoute from "./realm/Private";
 import Signup from "./realm/Signup";
 
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import UserList from "./components/userList";
+import Settings from "./components/settings";
+
+//import Home from './routes/Home';
+import Discover from './routes/Discover';
+import Data from './routes/Data'; 
+import SpecialEvent from './routes/Special';
+import Search from './routes/Search'; 
+
+import Calendar from './routes/Calendar';
+import Template from './routes/Template';
+//import Login from './routes/Login';
+import Safety from './routes/Safety';
+import Profile from './routes/Profile';
+import Contact from './routes/Contact';
+
+import Test from './routes/Test';
+import SearchLink from "./components/link_search"; 
+
 function App() {
  return (
    <BrowserRouter>
      <UserProvider>
+     <Navbar />
        <Routes>
-         <Route exact path="/login" element={<Login />} />
-         <Route exact path="/signup" element={<Signup />} />
-         <Route element={<PrivateRoute />}>
-           <Route exact path="/" element={<Home />} />
-         </Route>
+       <Route exact path="/" element={<Home />} />
+            <Route path='/discover' element={<Discover />} />
+            <Route path="/data/:id" element={<Data />} />
+            <Route path='/specialevent' element={<SpecialEvent/>} />
+            <Route path='/calendar' element={<Calendar/>} />
+            <Route path='/template' element={<Template/>} />
+            <Route path='/search' element={<Search />} /> {/* Use the Search component here */}
+            <Route path='/discover' element={<SearchLink />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/safety' element={<Safety />} />
+
+            <Route path='/test/:id' element={<Test />} />
+            
+            <Route path='/recordlist' element={<RecordList />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="/create" element={<Create />} />
+
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route element={<PrivateRoute />}>
+                <Route path='/profile/:user' element={<Profile />} />
+                <Route path='/contact/:user' element={<Contact />} />
+                <Route path='/userList' element={<UserList />} />
+                <Route path="/settings/:user" element={<Settings />} />
+            </Route>
        </Routes>
      </UserProvider>
    </BrowserRouter>
