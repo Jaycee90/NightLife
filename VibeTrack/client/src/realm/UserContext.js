0850e04b4c8ff1a2caa1145210a5e4fe10223872
Michelle Nguyen <rnb90@txstate.edu>
2023-10-24 11:56:27 -0500

    Fail: Update SignUp.js to create user with name and last name
    Unable to push name and lastname to database

--- VibeTrack/client/src/realm/UserContext.js
+++ VibeTrack/client/src/realm/UserContext.js
@@ -21,12 +21,12 @@ export const UserProvider = ({ children }) => {
  };
  
  // Function to sign up user into our App Service app using their email & password
- const emailPasswordSignup = async (email, password) => {
+ const emailPasswordSignup = async (email, password, name, lastName) => {
    try {
-     await app.emailPasswordAuth.registerUser(email, password);
+     await app.emailPasswordAuth.registerUser(email, password, name, lastName);
      // Since we are automatically confirming our users, we are going to log in
      // the user using the same credentials once the signup is complete.
-     return emailPasswordLogin(email, password);
+     return emailPasswordLogin(email, password, name, lastName);
    } catch (error) {
      throw error;
    }
