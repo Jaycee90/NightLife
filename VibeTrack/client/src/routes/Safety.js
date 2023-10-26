7e495e27dfa0515b315d7c99984f0ffdee4aee6d
Isaiah Gage <frb32@txstate.edu>
2023-10-25 19:29:55 -0500

    added fucntion to send emails

--- VibeTrack/client/src/routes/Safety.js
+++ VibeTrack/client/src/routes/Safety.js
@@ -1,49 +1,60 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Safety() {
-    // Instead of entering clubs might create a view where users can see all club names and click each
-    // club they are visiting tha night
-    const [clubs, setClubs] = useState([]); // Array of clunb names
-    // Hold club names and a fucntion to set the club name
-    const [clubName, setClubName] = useState(""); // temp holder for club name
+  const [emailData, setEmailData] = useState({
+    to: '',
+    subject: '',
+    text: '',
+  });
 
-    // Update the club entered
-    const updateClubsEntered = (e) => {
-        setClubName(e.target.value);
-    }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEmailData({ ...emailData, [name]: value });
+  };
 
-    // Add the club to the list of clubs the user is going to tonight
-    const handleClubEntered = () => {
-        setClubs((prevClubs) => [...prevClubs, clubName]);
-        setClubName("");
-    }    
-    console.log(clubs);
-    return (
-        <div style={{'color':'#000000'}}>
-            <h1>Safety First</h1>
-            <div>
-                <h3>Current Date will go here</h3> 
-                <p>
-                    Select the clubs you are going tonight, and our app will send a notification of the clubs you choose to a contact of your choosing
-                </p>
-                <p>Enter the clubs you are going to tonight here!</p>
-                <input 
-                    type="text"
-                    placeholder="Enter club names"
-                    value={clubName}
-                    onChange={updateClubsEntered}
-                />
-                <button onClick={handleClubEntered}>Submit club</button>
-                <ul>
-                    {clubs.map((club, index) => (
-                        <li key={index}>{club}</li>
-                     ))}
-                </ul>
+  const sendEmail = () => {
+    console.log('sendEmail function called');
+    fetch('http://localhost:5050/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(emailData),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log('Email sent:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
-
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        type="email"
+        name="to"
+        placeholder="Recipient"
+        value={emailData.to}
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="subject"
+        placeholder="Subject"
+        value={emailData.subject}
+        onChange={handleInputChange}
+      />
+      <textarea
+        name="text"
+        placeholder="Message"
+        value={emailData.text}
+        onChange={handleInputChange}
+      />
+      <button onClick={sendEmail}>Send Email</button>
+    </div>
+  );
 }
 
 export default Safety;
--- VibeTrack/server/server.mjs
+++ VibeTrack/server/server.mjs
@@ -5,6 +5,7 @@ import records from "./routes/record.mjs";
 import users from "./routes/user.mjs";
 import cheerio from "cheerio";
 import axios from "axios";
+import nodemailer from 'nodemailer';
 
 const PORT = 5050;
 const app = express();
@@ -38,7 +39,34 @@ app.get('/scrape', async (req, res) => {
     res.status(500).send('Error scraping data');
   }
 });
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
 
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Use your email service
+    auth: {
+      user: 'isaiah12gage@gmail.com', // Replace with your email
+      pass: 'tziq eztk aiwr jcug', // Replace with your email password
+    },
+  });
+
+  const mailOptions = {
+    from: 'isaiah12gage@gmail.com',
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Email not sent');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent');
+    }
+  });
+});
 app.use("/record", records);
 app.use("/user", users);
 
