ac971055cfc8076c55ca6fe27abb370b154b8a13
Michelle Nguyen <rnb90@txstate.edu>
2023-10-25 21:06:17 -0500

    Note: Update metadata did not go through
    Need to modify user.mjs

--- VibeTrack/client/src/routes/Test.js
+++ VibeTrack/client/src/routes/Test.js
@@ -48,6 +48,37 @@ export default function Profile() {
   }, [params.code]);
 
 
+  function updateForm(value) {
+    return setForm(prev => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const editedUser = {
+      _id: form._id,
+      name: form.name,
+      lastName: form.lastName,
+      phone: form.phone,
+      email: form.email,
+      birthdate: form.birthdate,
+      gender: form.gender,
+      emergency1: form.emergency1,
+      emergency2: form.emergency2,
+    };
+
+    await fetch(`http://localhost:5050/user/profile/${params.code}`, {
+      method: "PATCH",
+      body: JSON.stringify(editedUser),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    window.alert("Information updated successfully!");
+  }
+
   const [menuCollapse] = useState(false)
   
   const { logOutUser } = useContext(UserContext);
@@ -64,32 +95,132 @@ export default function Profile() {
 
   return (
     <div>
-      <div className="grid-settings">
-        <div className="grid-settings-left">
-          <div id="header">
-            <ProSidebar collapsed={menuCollapse}>
-              <SidebarContent>
-                <Menu iconShape="square">
-                  <MenuItem active={true} icon={<FiHome />}>Profile</MenuItem>
-                  <MenuItem icon={<BiCog />}>Security</MenuItem>
-                  <MenuItem icon={<FaList />}>Contacts</MenuItem>
-                  <MenuItem icon={<FaRegHeart />}>Favorite</MenuItem>
-                  
-                </Menu>
-              </SidebarContent>
-              <SidebarFooter>
-                <Menu iconShape="square">
-                  <MenuItem icon={<FiLogOut />}  onClick={logOut}>Logout</MenuItem>
-                </Menu>
-              </SidebarFooter>
-            </ProSidebar>
-          </div>
-        </div>
-        <div className="grid-settings-right">
-          <h3 style={{ color: '#000000', paddingBottom: '10px' }}>General Information</h3>
-          <h3 style={{ color: '#000000', paddingBottom: '10px' }}>{form.name}</h3>
+    <div className="grid-settings">
+      <div className="grid-settings-left">
+        <div id="header">
+          <ProSidebar collapsed={menuCollapse}>
+            <SidebarContent>
+              <Menu iconShape="square">
+                <MenuItem active={true} icon={<FiHome />}>Profile</MenuItem>
+                <MenuItem icon={<BiCog />}>Security</MenuItem>
+                <MenuItem icon={<FaList />}>Contacts</MenuItem>
+                <MenuItem icon={<FaRegHeart />}>Favorite</MenuItem>
+                
+              </Menu>
+            </SidebarContent>
+            <SidebarFooter>
+              <Menu iconShape="square">
+                <MenuItem icon={<FiLogOut />}  onClick={logOut}>Logout</MenuItem>
+              </Menu>
+            </SidebarFooter>
+          </ProSidebar>
         </div>
       </div>
+      <div className="grid-settings-right">
+        <h3 style={{ color: '#000000', paddingBottom: '10px' }}>General Information</h3>
+        <form onSubmit={onSubmit} style={{ color: '#000000' }}>
+          <div className="grid-about">
+            <div className="item">
+              <div className="form-group">
+                <label htmlFor="name">First Name: </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  value={form.name}
+                  onChange={(e) => updateForm({ name: e.target.value })}
+                />
+              </div>
+            </div>
+            <div className="item">
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name: </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  value={form.lastName}
+                  onChange={(e) => updateForm({ lastName: e.target.value })}
+                />
+              </div>
+            </div>
+            <div className="item">
+              <div className="form-group">
+                <label htmlFor="gender" style={{ color: 'black' }}>Gender: </label>
+                <select
+                  className="form-control"
+                  id="gender"
+                  value={form.gender}
+                  onChange={(e) => updateForm({ gender: e.target.value })}
+                  style={{ backgroundColor: '#747474', borderRadius: '25px', color: 'white', height: '40px', padding: '10px', paddingLeft: '30px', width: '100%', fontSize: '17px' }}
+                >
+                  <option value="Female">Female</option>
+                  <option value="Male">Male</option>
+                  <option value="Rather not disclose">Rather not disclose</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="item">
+              <div className="form-group">
+                <label htmlFor="phone">Phone: </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phone"
+                  value={form.phone}
+                  onChange={(e) => updateForm({ phone: e.target.value })}
+                />
+              </div>
+            </div>
+            <div className="item">
+              <div className="form-group">
+                <label htmlFor="birthdate">Birthdate: </label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="birthdate"
+                  value={form.birthdate}
+                  onChange={(e) => updateForm({ birthdate: e.target.value })}
+                  style={{ backgroundColor: '#747474', borderRadius: '25px', color: 'white', height: '40px', padding: '10px', paddingLeft: '30px', width: '100%', fontSize: '17px' }}
+                />
+              </div>
+            </div>
+            <div className="item">
+              <div className="form-group">
+                <label htmlFor="_id">ID (Read Only): </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="_id"
+                  value={form._id}
+                  readOnly
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              value={form.email}
+              onChange={(e) => updateForm({ email: e.target.value })}
+            />
+          </div>
+          <br />
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Update Information"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
   );
 }
