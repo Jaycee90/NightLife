324a368906df7e359d84c75d5975dc7c2f0fc6f5
Michelle Nguyen <rnb90@txstate.edu>
2023-10-27 18:21:26 -0500

    Startpoint: Implementing Security.js

---
+++ VibeTrack/client/src/realm/Security.js
@@ -0,0 +1,5 @@
+import { Button, TextField } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
+import '../css/login.css';
