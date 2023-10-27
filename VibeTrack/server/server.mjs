185e01ec6829a4569429c2aa46c3fe129bd76ef5
Michelle Nguyen <rnb90@txstate.edu>
2023-10-27 11:26:56 -0500

    Got webscrape to work on its own route

--- VibeTrack/server/server.mjs
+++ VibeTrack/server/server.mjs
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
-import users from "./routes/user.mjs";
-import scrape from "./routes/scrape.mjs";
+import record from "./routes/record.mjs";
+import user from "./routes/user.mjs";
+import { scrapeData } from "./routes/scrape.mjs"; // Import the scrapeData function
 
 const PORT = 5050;
 const app = express();
@@ -11,9 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/record", records);
-app.use("/user", users);
-app.use("/scrape", scrape);
+app.use("/record", record);
+app.use("/user", user);
+
+app.get('/scrape', async (req, res) => {
+  try {
+    const eventInfo = await scrapeData(); // Use the scrapeData function
+    res.json(eventInfo); // Send structured data as JSON response
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 // start the Express server
 app.listen(PORT, () => {
