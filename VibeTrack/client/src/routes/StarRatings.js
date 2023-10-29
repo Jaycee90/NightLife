62b60331dbc203d929cdbd823c294e3421a665ea
Benu Liburd <bjl98@txstate.edu>
2023-10-29 12:56:53 -0500

    added possible colors for the star rating
    can be changed later

--- VibeTrack/client/src/routes/StarRatings.js
+++ VibeTrack/client/src/routes/StarRatings.js
@@ -1,2 +1,9 @@
 import { useState } from "react";
 import '../css/StarRating.css';
+import { FaStar } from "react-icons/fa";
+
+const colors = {
+    orange: "#FFBA5A",
+    grey: "#a9a9a9"
+    
+};
