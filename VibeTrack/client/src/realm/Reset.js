e0731e228a4f39a4135636356a9b11e635620981
Michelle Nguyen <rnb90@txstate.edu>
2023-10-29 14:15:41 -0500

    Added comments

--- VibeTrack/client/src/realm/Reset.js
+++ VibeTrack/client/src/realm/Reset.js
@@ -4,26 +4,34 @@ import { UserContext } from "./UserContext";
 import '../css/login.css';
 
 const Reset = () => {
+    // State to manage the form input
     const [form, setForm] = useState({ 
         password: "",
     });
 
+    // Accessing the passwordReset function from the context
     const { passwordReset } = useContext(UserContext);
 
+    // Handler for form input changes
     const onFormInputChange = (event) => {
         const { name, value } = event.target;
         setForm({ ...form, [name]: value });
     };
 
+    // Function to reset password with the provided token and tokenId
     const resetPasswordWithToken = async () => {
         const urlParams = new URLSearchParams(window.location.search);
         const token = urlParams.get('token');
         const tokenId = urlParams.get('tokenId');
 
         try {
+            // Call the passwordReset function with the provided password, token, and tokenId
             await passwordReset(form.password, token, tokenId);
+
+            // Display a success message
             alert("Password reset successfully!");
         } catch (error) {
+            // Handle any errors that may occur during the process
             alert(error.message);
         }
     };
@@ -31,6 +39,7 @@ const Reset = () => {
     return (
         <form>
             <h1>Reset Password</h1>
+            {/* Input field for the new password */}
             <TextField
                 label="New Password"
                 type="password"
@@ -39,6 +48,7 @@ const Reset = () => {
                 value={form.password}
                 onChange={onFormInputChange}
             />
+            {/* Button to trigger the password reset */}
             <Button variant="contained" color="primary" onClick={resetPasswordWithToken}>
                 Reset Password
             </Button>
