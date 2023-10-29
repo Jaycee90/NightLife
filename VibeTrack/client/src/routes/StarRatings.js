cf2c2a28b49fcd0b1900d4a730b807ff4dd62060
Benu Liburd <bjl98@txstate.edu>
2023-10-29 13:38:45 -0500

    added style code from test app.

--- VibeTrack/client/src/routes/StarRatings.js
+++ VibeTrack/client/src/routes/StarRatings.js
@@ -60,3 +60,31 @@ function StarRating() {
     </div>
     );
 };
+
+
+const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    },
+    stars: {
+      display: "flex",
+      flexDirection: "row",
+    },
+    textarea: {
+      border: "1px solid #a9a9a9",
+      borderRadius: 5,
+      padding: 10,
+      margin: "20px 0",
+      minHeight: 100,
+      width: 300
+    },
+    button: {
+      border: "1px solid #a9a9a9",
+      borderRadius: 5,
+      width: 300,
+      padding: 10,
+    }
+  
+  };
