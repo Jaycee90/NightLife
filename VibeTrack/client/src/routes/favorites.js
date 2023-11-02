4f59024ac7aa1c08a553d98bd17779704a2e5f51
Benu Liburd <bjl98@txstate.edu>
2023-11-02 15:52:13 -0500

    testing issues #3

--- VibeTrack/client/src/routes/favorites.js
+++ VibeTrack/client/src/routes/favorites.js
@@ -1,114 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
 function Favorites() {
-  const [venues, setVenues] = useState([]); // array of venue objects
-  const [selectedVenues, setSelectedVenues] = useState([]); // array of venues to be sent
-  const [showFavoritesOnly, setShowFavoritesOnly] = useState(false); // new state for toggling favorites
-  const [confirmationVenue, setConfirmationVenue] = useState(null); // confirmation state
+  const [venues, setVenues] = useState({ name: "" });
 
   useEffect(() => {
-    // Retrieve all of the venues
-    const getVenues = async () => {
-      try {
-        const response = await fetch(`http://localhost:5050/record/`);
-
-        if (!response.ok) {
-          const message = `An error occurred: ${response.statusText}`;
-          window.alert(message);
-          return;
-        }
-
-        const venueData = await response.json();
-        setVenues(venueData);
-      } catch (error) {
-        console.log('Error fetching venues from the database, ', error);
-      }
-    };
-    getVenues();
+    // Call the function to fetch and set venues
+    fetchAndSetVenues();
   }, []);
 
-  const handleClickedVenue = (clickedVenue) => {
-    if (selectedVenues.includes(clickedVenue)) return;
-    setSelectedVenues([...selectedVenues, clickedVenue]);
-  };
+  const fetchAndSetVenues = async () => {
+    try {
+      const response = await fetch(`http://localhost:5050/record/`);
 
-  const toggleFavoritesDisplay = () => {
-    setShowFavoritesOnly(!showFavoritesOnly); // Toggle the state
-  };
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
 
-  // Function to add the current venue to favorites
-  const addToFavorites = (venue) => {
-    if (!selectedVenues.includes(venue.name)) {
-      setSelectedVenues([...selectedVenues, venue.name]);
+      const venueData = await response.json();
+      setVenues({ name: venueData });
+    } catch (error) {
+      console.log('Error fetching venues from the database, ', error);
     }
-  };
-
-  const requestRemoveConfirmation = (venue) => {
-    setConfirmationVenue(venue);
-  };
-
-  const cancelRemoveConfirmation = () => {
-    setConfirmationVenue(null);
-  };
-
-  const confirmRemoveFromFavorites = (venue) => {
-    setSelectedVenues(selectedVenues.filter((v) => v !== venue));
-    setConfirmationVenue(null); // Clear the confirmation
   };
 
   return (
     <div>
       <h1>Selected Venues</h1>
-      <ul>
-        {selectedVenues.map((venue, index) => (
-          <li key={index} style={{ color: 'blue' }}>
-            {venue}
-            <button onClick={() => requestRemoveConfirmation(venue)}>
-              Remove from Favorites
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {confirmationVenue && (
-        <div>
-          <p>Are you sure you want to remove {confirmationVenue} from favorites?</p>
-          <button onClick={() => confirmRemoveFromFavorites(confirmationVenue)}>
-            Yes
-          </button>
-          <button onClick={cancelRemoveConfirmation}>No</button>
-        </div>
+      {/* Render venue names here using venues.name */}
+      {venues.name && (
+        <ul style={{ color: 'blue' }}>
+          {venues.name.map((venueName, index) => (
+            <li key={index}>{venueName}</li>
+          ))}
+        </ul>
       )}
-
-      <button onClick={toggleFavoritesDisplay}>
-        {showFavoritesOnly ? 'Show All Venues' : 'Show Favorites Only'}
-      </button>
-
-      <ul style={{ color: '#000' }}>
-        {showFavoritesOnly
-          ? selectedVenues.map((venue, index) => (
-              <li key={index} style={{ color: 'blue' }}>
-                {venue}
-                <button onClick={() => requestRemoveConfirmation(venue)}>
-                  Remove from Favorites
-                </button>
-              </li>
-            ))
-          : venues.map((venue, index) => (
-              <li key={index} style={{ color: 'blue' }}>
-                {venue.name}
-                {!selectedVenues.includes(venue.name) && (// Conditionally render the button
-                  <button onClick={() => addToFavorites(venue)}>
-                    Add to Favorites
-                  </button>
-                )}
-              </li>
-            ))}
-      </ul>
     </div>
   );
 }
 
 export default Favorites;
-
-// at a stopping point
