f1138588d6060630f6cc1d971685ce950a711925
Benu Liburd <bjl98@txstate.edu>
2023-11-02 02:11:51 -0500

    attempt to display all the venues
    this is for my sake, it will eventually turn into the page that will display the users updated favorites. it should wipe them from their favorites list if they unadd it as well.

--- VibeTrack/client/src/routes/favorites.js
+++ VibeTrack/client/src/routes/favorites.js
@@ -1,13 +1,55 @@
-//created a file for the favorites function.
-
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
 function Favorites() {
-    return (
-        <div>
-            <h1>This is a test message for the favorites page</h1>
-        </div>
-    );
-};
+  const [venues, setVenues] = useState([]); // array of venue names
+  const [selectedVenues, setSelectedVenues] = useState([]); // array of venues to be sent
 
-export default Favorites;  
+  useEffect(() => {
+    // Retrieve all of the venue names
+    const getVenueNames = async () => {
+      try {
+        const response = await fetch('http://localhost:5050/record/names');
+
+        if (!response.ok) {
+          const message = `An error occurred: ${response.statusText}`;
+          window.alert(message);
+          return;
+        }
+
+        const venueData = await response.json();
+        setVenues(venueData.names);
+      } catch (error) {
+        console.log('Error fetching venue names from the database,', error);
+      }
+    };
+    getVenueNames();
+  }, []);
+
+  const handleClickedVenue = (clickedVenue) => {
+    if (selectedVenues.includes(clickedVenue)) return;
+    else {
+      // Add the new venue to the list
+      setSelectedVenues([...selectedVenues, clickedVenue]);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Selected Venues</h1>
+      <ul>
+        {selectedVenues.map((venue, index) => (
+          <li key={index}>{venue}</li>
+        ))}
+      </ul>
+      <ul style={{ color: '#000' }}>
+        {venues.map((venue, index) => (
+          <li key={index} onClick={() => handleClickedVenue(venue)}>
+            {venue}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Favorites;
