49c2f5678c0b7d22c9a8eb142f39bd2916380c01
Benu Liburd <bjl98@txstate.edu>
2023-11-02 01:15:17 -0500

    created a sample text for the favorites page.

--- VibeTrack/client/src/routes/favorites.js
+++ VibeTrack/client/src/routes/favorites.js
@@ -1,1 +1,13 @@
 //created a file for the favorites function.
+
+import React from "react";
+
+function Favorite() {
+    return (
+        <div>
+            <h1>This is a test message for the favorites page</h1>
+        </div>
+    );
+};
+
+export default Calendar; 
