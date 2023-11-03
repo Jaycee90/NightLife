9e2354a176584f1bb57956fa121e8d09876b3222
Michelle Nguyen <rnb90@txstate.edu>
2023-11-03 18:08:13 -0500

    Attempt to resolve merge conflict with SCRUM 85

--- VibeTrack/client/src/routes/Data.js
+++ VibeTrack/client/src/routes/Data.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; //Something
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useParams } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
