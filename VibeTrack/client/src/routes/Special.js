748fb027653f47bf3dbc2bc384eee9be94aa9b19
Michelle Nguyen <rnb90@txstate.edu>
2023-11-04 18:34:58 -0500

    Added function to trim webscraped event

--- VibeTrack/client/src/routes/Special.js
+++ VibeTrack/client/src/routes/Special.js
@@ -3,38 +3,54 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function SpecialEvent() {
-  const [eventData, setEventData] = useState([]); // Hold events while live scrapping
-  // When a user clicks on the special event link, this function will be the first thing that happens
-  // Making a request to out local host where our stored data is held then bring that data back here
-  
+  const [eventData, setEventData] = useState([]);
+
+  function extractEventData(eventText) {
+    const parts = eventText.split(' - ');
+
+    if (parts.length === 2) {
+      const venue = parts[0].trim();
+      const eventContent = parts[1].match(/^(.*?)\s\((.*?)\)$/);
+
+      if (eventContent) {
+        const eventName = eventContent[1].trim();
+        const time = eventContent[2].trim();
+
+        return { venue, eventName, time };
+      }
+    }
+
+    return null;
+  }
+
   useEffect(() => {
-    axios.get('http://localhost:5050/scrape') // Adjust the URL to match your server's address
+    axios.get('http://localhost:5050/scrape')
       .then((response) => {
-        setEventData(response.data);
+        const formattedEventData = response.data.map(event => extractEventData(event.divText)).filter(Boolean);
+        setEventData(formattedEventData);
       })
       .catch((error) => {
         console.error('Error fetching event data', error);
       });
   }, []);
 
-  
-    return (
-        <div class="event-container">
-           <h1 style={{color:'#000'}}>Upcoming Events</h1>
-      <ul style={{color:'#000'}}>
-        {eventData.map((event, index) => ( 
+  return (
+    <div className="event-container">
+      <h1 style={{ color: '#000' }}>Upcoming Events</h1>
+      <ul style={{ color: '#000' }}>
+        {eventData.map((event, index) => (
           <li key={index}>
-            <div class="event">
-            <div class="event-left"></div>
-            <div class="event-date"> 
-              <p class="event-description">{event.day} {event.divText}</p>
-            </div>
+            <div className="event">
+              <div className="event-left"></div>
+              <div className="event-date">
+                <p className="event-description">{`${event.venue} - ${event.eventName} (${event.time})`}</p>
+              </div>
             </div>
           </li>
         ))}
       </ul>
-      </div>
-      );
-};
+    </div>
+  );
+}
 
 export default SpecialEvent;
