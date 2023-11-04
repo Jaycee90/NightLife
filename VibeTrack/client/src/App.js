df44e85da01698fa02bd987711645caf7d617560
Michelle Nguyen <rnb90@txstate.edu>
2023-11-03 20:22:02 -0500

    Attempt to resolve merge conflict

--- VibeTrack/client/src/App.js
+++ VibeTrack/client/src/App.js
@@ -25,6 +25,7 @@ import Safety from './routes/Safety';
 import Favorites from "./routes/favorites";
 import Profile from './realm/Profile';
 import Contact from './realm/Contact';
+import Favorites from "./routes/favorites";
 
 import Home from './routes/Home';
 import SearchLink from "./components/link_search";
