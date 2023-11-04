df0e1cb0c2e1598118a8a7be6f6ccce3d411ce1c
Michelle Nguyen <rnb90@txstate.edu>
2023-11-03 20:18:14 -0500

    Attempt to resolve merge conflict

--- VibeTrack/client/src/App.js
+++ VibeTrack/client/src/App.js
@@ -12,6 +12,8 @@ import Edit from "./components/edit";
 import Create from "./components/create";
 import UserList from "./components/userList";
 
+import StarRating from "./routes/StarRatings";
+
 import Discover from './routes/Discover';
 import Data from './routes/Data'; 
 import SpecialEvent from './routes/Special';
@@ -20,9 +22,9 @@ import Search from './routes/Search';
 import Calendar from './routes/Calendar';
 import Template from './routes/Template';
 import Safety from './routes/Safety';
-import Profile from './routes/Profile';
-import Contact from './routes/Contact';
 import Favorites from "./routes/favorites";
+import Profile from './realm/Profile';
+import Contact from './realm/Contact';
 
 import Home from './routes/Home';
 import SearchLink from "./components/link_search";
@@ -45,21 +47,22 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/safety' element={<Safety />} />
             <Route path='/recordlist' element={<RecordList />} />
-            <Route path="/edit/:ID" element={<Edit />} />
+            <Route path="/edit/:id" element={<Edit />} />         
+            <Route path='/favorites' element={<Favorites/>} />
             <Route path="/create" element={<Create />} />
             <Route path='/invitation' element={<Invitation />} />
-            <Route path='/favorites' element={<Favorites/>} />
+            <Route path='/rating/:id' element={<StarRating />} />   
+            
 
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route element={<PrivateRoute />}>
+                <Route path='/profile/' element={<Profile />} />
+                <Route path='/contact/' element={<Contact />} />
+                <Route path='/userList' element={<UserList />} />
+            </Route>
             <Route path='/security' element={<Security />} />
             <Route path='/reset' element={<Reset />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route element={<PrivateRoute />}>
-                <Route path='/profile/:code' element={<Profile />} />
-                <Route path='/contact/:code' element={<Contact />} />
-                <Route path='/userList' element={<UserList />} />
-
-            </Route>
        </Routes>
      </UserProvider>
    </BrowserRouter>
