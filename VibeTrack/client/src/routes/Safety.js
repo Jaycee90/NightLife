35ac584b1f89aa16d8aaf2f4a47c9b2f08b1e83e
Michelle Nguyen <rnb90@txstate.edu>
2023-11-05 17:19:05 -0600

    Stoppoint: work from sample found for multiple text input
    npm i @material-ui/core --legacy-peer-deps https://codesandbox.io/s/multiple-textinputs-ojiv3?fontsize=12&hidenavigation=1&theme=dark&file=/src/App.js:0-1719

--- VibeTrack/client/src/routes/Safety.js
+++ VibeTrack/client/src/routes/Safety.js
@@ -2,6 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { Chip, FormControl, Input, makeStyles,} from "@material-ui/core";
 import '../css/safety.css';
 
+const useStyles = makeStyles((theme) => ({
+  formControlRoot: {
+    display: "flex",
+    alignItems: "center",
+    gap: "8px",
+    width: "300px",
+    flexWrap: "wrap",
+  flexDirection: "row",
+  border:'2px solid lightgray',
+  padding:4,
+  borderRadius:'4px',
+    "&> div.container": {
+      gap: "6px",
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap"
+    },
+    "& > div.container > span": {
+      backgroundColor: "gray",
+      padding: "1px 3px",
+      borderRadius: "4px"
+    }
+  }
+}));
+
 function Safety() {
   const [venues, setVenues] = useState([]);
   const [selectedVenues, setSelectedVenues] = useState([]);
@@ -11,6 +36,27 @@ function Safety() {
     text: '',
   });
 
+  const classes = useStyles();
+  const [values, setValues] = useState(["test"]);
+  const [currValue, setCurrValue] = useState("");
+  const handleKeyUp = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode == 13) {
+      setValues((oldState) => [...oldState, e.target.value]);
+      setCurrValue("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setCurrValue(e.target.value);
+  };
+
+  const handleDelete = ( item, index) =>{
+    let arr = [...values]
+    arr.splice(index,1)
+    console.log(item)
+    setValues(arr)
+  }
   useEffect(() => {
     const getVenues = async () => {
       try {
@@ -76,6 +122,18 @@ function Safety() {
         <p className="intro-description">We are here for you to ease the minds of anyone else when you go out tonight! </p>
         <p className="intro-description">Choose from a list of great clubs you are visiting, and we'll drop them a message or alert them if something goes wrong.</p>
       </div>
+      <div>			  <FormControl classes={{ root: classes.formControlRoot }}>
+				  <div className={"container"}>
+					  {values.map((item,index) => (
+						  <Chip  size="small" onDelete={()=>handleDelete(item,index)} label={item}/>
+					  ))}
+				  </div>
+				  <Input
+					  value={currValue}
+					  onChange={handleChange}
+					  onKeyDown={handleKeyUp}
+				  />
+			  </FormControl></div>
       <div className="selected-clubs-container">
         <h1 className="selected-clubs-title">Selected Clubs</h1>
         <ul className="selected-clubs-list">
