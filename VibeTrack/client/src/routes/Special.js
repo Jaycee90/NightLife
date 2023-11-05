73901d99e2bd8608311e6b78a51e6b24652bf61c
Michelle Nguyen <rnb90@txstate.edu>
2023-11-04 19:21:50 -0500

    Added function to parse event.day and event.divText

--- VibeTrack/client/src/routes/Special.js
+++ VibeTrack/client/src/routes/Special.js
@@ -3,61 +3,65 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function SpecialEvent() {
-  const [eventData, setEventData] = useState([]);
-
-  function extractEventData(eventText) {
-    const parts = eventText.split(' - ');
-
-    if (parts.length === 2) {
-      const venue = parts[0].trim();
-      const eventContent = parts[1].match(/^(.*?)\s\((.*?)\)$/);
-
-      if (eventContent) {
-        const eventName = eventContent[1].trim();
-        const time = eventContent[2].trim();
-
-        return { venue, eventName, time };
-      }
-    }
-
-    return null;
-  }
+  const [eventData, setEventData] = useState([]); // Hold events while live scrapping
 
   useEffect(() => {
     axios.get('http://localhost:5050/scrape')
       .then((response) => {
-        const formattedEventData = response.data.map(event => extractEventData(event.divText)).filter(Boolean);
-        setEventData(formattedEventData);
+        setEventData(response.data);
       })
       .catch((error) => {
         console.error('Error fetching event data', error);
       });
   }, []);
 
+  const parseDivText = (divText) => {
+    const parts = divText.split(' - ');
+    if (parts.length !== 2) {
+      return { venue: '', eventName: '', time: '' }; // Return empty values if format is incorrect
+    }
+    const [venue, rest] = parts;
+    const [eventName, time] = rest.split(' (');
+    if (!eventName || !time) {
+      return { venue: '', eventName: '', time: '' }; // Return empty values if format is incorrect
+    }
+    return { venue, eventName, time: time.slice(0, -1) }; // Remove the trailing ')'
+  };
+  
+  const parseEventDay = (eventDay) => {
+    const parts = eventDay.split(' - ');
+    if (parts.length !== 2) {
+      return { day: '', month: '', date: '' }; // Return empty values if format is incorrect
+    }
+    const [day, date] = parts;
+    const [monthName, dayOfMonth] = date.split(' ');
+    if (!monthName || !dayOfMonth) {
+      return { day: '', month: '', date: '' }; // Return empty values if format is incorrect
+    }
+    return { day, month: monthName, date: dayOfMonth };
+  };
+
   return (
     <div className="event-container">
-      <h1 style={{ color: '#000' }}>Upcoming Events</h1>
-      <ul style={{ color: '#000' }}>
-        {eventData.map((event, index) => (
-          <li key={index}>
-                  <div class="event">
-              <div class="event-left">
-                <div class="event-date">{event.day}
-                  {/*
-                  <div class="date">22</div>
-                  <div class="month">Dec</div> */}
-                </div>
+      <h1 style={{color:'#000'}}>Upcoming Events</h1>
+      
+      <ul style={{color:'#000'}}>
+        {eventData.map((event, index) => { 
+          const { venue, eventName, time } = parseDivText(event.divText);
+          const { day, month, date } = parseEventDay(event.day);
+          return (
+            <li key={index}>
+              <div className="event"> </div>
+              <div className="event-left"> </div>
+              <div className="event-date"> 
+                <h1>Date: {day} - Month {month}  Date {date}</h1>
+                <p className="event-description">Venue: {venue}</p>
+                <p className="event-description">Event: {eventName}</p>
+                <p className="event-description">Time: {time}</p>
               </div>
-              <div class="event-right">
-                <h3 class="event-title">{event.venue}</h3>
-                <div class="event-description">{event.eventName}</div>
-                <div class="event-timing">
-                  <img src="https://i.imgur.com/P0ajz6E.png" alt="" /> {event.time}
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
