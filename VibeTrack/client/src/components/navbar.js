8549d7519c189f0440ceec2b27d59083585e2f3f
Michelle Nguyen <rnb90@txstate.edu>
2023-11-05 15:36:44 -0600

    Stoppoint
    Dealing with navbar again

--- VibeTrack/client/src/components/navbar.js
+++ VibeTrack/client/src/components/navbar.js
@@ -4,7 +4,7 @@ import '../css/navbar.css';
 export default function Navbar() {
  return (
    <div className="navbar-content" >
-      <div class="navigation-bar" style={{marginBottom:"20px"}}>
+      <div class="navigation-bar">
         <a href="/" class="split"><img src="https://i.imgur.com/KIpIJ7M.png" alt="Logo" loading="lazy" style={{ height:"30px"}} /></a>
         <a href="login">Login</a>
         <a href="specialevent">Special Event</a>
