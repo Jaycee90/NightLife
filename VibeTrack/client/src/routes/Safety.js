0345044375ffa61b817f6a66bcae30cf9fd0decb
Michelle Nguyen <rnb90@txstate.edu>
2023-11-05 17:27:28 -0600

    Stoppoint
    formatting input field

--- VibeTrack/client/src/routes/Safety.js
+++ VibeTrack/client/src/routes/Safety.js
@@ -5,22 +5,22 @@ import '../css/safety.css';
 const useStyles = makeStyles((theme) => ({
   formControlRoot: {
     display: "flex",
+    backgroundColor: "#fff",
     alignItems: "center",
     gap: "8px",
-    width: "300px",
     flexWrap: "wrap",
   flexDirection: "row",
-  border:'2px solid lightgray',
   padding:4,
   borderRadius:'4px',
     "&> div.container": {
       gap: "6px",
       display: "flex",
       flexDirection: "row",
+      backgroundColor: "#fff",
       flexWrap: "wrap"
     },
     "& > div.container > span": {
-      backgroundColor: "gray",
+      backgroundColor: "#fff",
       padding: "1px 3px",
       borderRadius: "4px"
     }
@@ -41,7 +41,7 @@ function Safety() {
   const [currValue, setCurrValue] = useState("");
   const handleKeyUp = (e) => {
     console.log(e.keyCode);
-    if (e.keyCode == 13) {
+    if (e.keyCode === 13) {
       setValues((oldState) => [...oldState, e.target.value]);
       setCurrValue("");
     }
@@ -122,18 +122,23 @@ function Safety() {
         <p className="intro-description">We are here for you to ease the minds of anyone else when you go out tonight! </p>
         <p className="intro-description">Choose from a list of great clubs you are visiting, and we'll drop them a message or alert them if something goes wrong.</p>
       </div>
-      <div>			  <FormControl classes={{ root: classes.formControlRoot }}>
+      <div>	
+        <FormControl classes={{ root: classes.formControlRoot }}>
 				  <div className={"container"}>
 					  {values.map((item,index) => (
-						  <Chip  size="small" onDelete={()=>handleDelete(item,index)} label={item}/>
+						  <Chip  size="small" onDelete={()=>handleDelete(item,index)} label={item} style={{backgroundColor:'#747474', color:'#fff'}}/>
 					  ))}
 				  </div>
 				  <Input
 					  value={currValue}
 					  onChange={handleChange}
 					  onKeyDown={handleKeyUp}
+            placeholder="Enter email"
+            style={{background:'#fff', color:'#747474', width:'200px'}}
+            inputProps={{ style: { backgroundColor: "#fff", color:'#747474' } }}
 				  />
 			  </FormControl></div>
+
       <div className="selected-clubs-container">
         <h1 className="selected-clubs-title">Selected Clubs</h1>
         <ul className="selected-clubs-list">
