c89e4f5120f691f769a69b4f38e7fa8206fd2d60
Michelle Nguyen <rnb90@txstate.edu>
2023-11-09 13:43:33 -0600

    Add placeholder for search component on Discover

--- VibeTrack/client/src/routes/Discover.js
+++ VibeTrack/client/src/routes/Discover.js
@@ -5,6 +5,8 @@ import '../css/discover.css';
 const Discover = () => {
   const [venueData, setVenueData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [records, setRecords] = useState([]); 
+  const [searchQuery, setSearchQuery] = useState('');//state variable to hold the search query.
 
   useEffect(() => {
     async function getVenues() {
@@ -51,10 +53,48 @@ const Discover = () => {
     </li>
   ));
 
+  useEffect(() => { 
+    async function getRecords() { // Define an function to fetch data
+      // Send a GET request to the server 
+      const response = await fetch(`http://localhost:5050/record/`);
+
+      if (!response.ok) { // Check if the response is successful
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const records = await response.json(); // Parse the response (object in database) as JSON
+
+      setRecords(records);  // Update the 'record' state with the fetched data
+    }
+
+    getRecords();  // Call fetchData function
+
+    return;
+  }, [records.length]);
+
+    // Function to update the search query
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    };
+    
+
   return (
     <div className="discover-component" style={{marginBottom:'40px'}}>
       <p className="section-subtitle" >Discover all nightclubs and venues in the San Marcos area </p>
       <h2 className="h2 section-title">Discover venues</h2>
+      <div className="grid-discover-search">
+        <div class="item">
+        <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={handleSearch}
+                        placeholder="Search by venue name"
+                        style={{borderRadius:"10px", height:"40px", background:'#fff', color:'#747474'}}
+                    />
+        </div>
+        <div class="item">
       <div class="container" style={{paddingTop:'40px', paddingLeft:'20px', paddingRight:'20px'}}>
           <ul className="discover-list" >
           {currentVenues.map((venueData) => (
@@ -84,6 +124,8 @@ const Discover = () => {
             {renderPageNumbers}
         </ul>
     </div>
+        </div>
+      </div>
     </div>
   );
 };
