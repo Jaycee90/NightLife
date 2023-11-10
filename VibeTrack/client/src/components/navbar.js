3a8fdc76c639222ed2c7634563aa8db054caaa41
Michelle Nguyen <rnb90@txstate.edu>
2023-11-09 18:20:38 -0600

    Added search to navigation bar

--- VibeTrack/client/src/components/navbar.js
+++ VibeTrack/client/src/components/navbar.js
@@ -6,13 +6,21 @@ export default function Navbar() {
    <div className="navbar-content" >
       <div class="navigation-bar">
         <a href="/" class="split"><img src="https://i.imgur.com/KIpIJ7M.png" alt="Logo" loading="lazy" style={{ height:"30px"}} /></a>
-        <a href="profile">Profile</a>
-        <a href="favorites">Favorite</a>
+        
+        {/*eslint-disable-next-line*/} 
+        <a> <input 
+              type="text" 
+              placeholder="Search for venues..."
+              style={{ backgroundColor:"#fff" , color:'#747474', paddingBottom:'10px', marginTop:'0px', borderRadius:'10px'}}
+              inputProps={{ style: { backgroundColor: "#fff", color:'#747474'} }}
+              /></a>
         <a href="specialevent">Special Event</a>
         <a href="search">Search</a>
         <a href="invitation">Invitation</a>
         <a href="safety">Safety</a>
         <a href="discover">Discover</a>
+        
+       
       </div>
     </div>
  );
