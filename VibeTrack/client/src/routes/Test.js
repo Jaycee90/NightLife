6ef38172a3a70fd07b1cb5ba23fbc1c17c80a104
Michelle Nguyen <rnb90@txstate.edu>
2023-11-09 20:35:26 -0600

    Stoppoint
    Back to work on test.js

--- VibeTrack/client/src/routes/Test.js
+++ VibeTrack/client/src/routes/Test.js
@@ -9,38 +9,51 @@ function Test() {
 
   const handleSearch = async () => {
     try {
+      // Send a request to the backend API for a search query
       const response = await fetch(`http://localhost:5050/record/search/${searchQuery}`);
+      
+      // Check if the response is successful (status code 2xx)
       if (!response.ok) {
+        // If there's an error, throw a custom Error with a descriptive message
         throw new Error(`An error has occurred: ${response.statusText}. ${searchQuery}`);
       }
+  
+      // Parse the response as JSON and update the 'results' state
       const data = await response.json();
       setResults(data);
-    } 
-    catch (error) {
+    } catch (error) {
+      // If an error occurs during the fetch or parsing, handle it here
       console.error(error);
-      window.alert(error.message);
+      window.alert(error.message); // Display an alert with the error message
     }
   };
-
+  
   useEffect(() => {
     if (params.id) {
       async function fetchData() {
         try {
+          // Send a request to the backend API for a specific record by ID
           const response = await fetch(`http://localhost:5050/record/${params.id}`);
+          
+          // Check if the response is successful (status code 2xx)
           if (!response.ok) {
+            // If there's an error, throw a custom Error with a descriptive message
             throw new Error(`An error has occurred: ${response.statusText}`);
           }
+  
+          // Parse the response as JSON and updateW the 'results' state
           const data = await response.json();
           setResults(data);
         } catch (error) {
+          // If an error occurs during the fetch or parsing, handle it here
           console.error(error);
-          window.alert(error.message);
+          window.alert(error.message); // Display an alert with the error message
         }
       }
       fetchData();
     }
   }, [params.id]);
-
+  
   return (
     <div style={{ padding: '20px' }}>
       <input
