06865c67ca3970b7d1906de5a0dbf594cb111354
Michelle Nguyen <rnb90@txstate.edu>
2023-11-10 12:59:00 -0600

    Stoppoint
    Removed searchbar. Need to find alternative

--- VibeTrack/client/src/routes/Test.js
+++ VibeTrack/client/src/routes/Test.js
@@ -1,12 +1,136 @@
-import React from 'react';
-import SearchBar from '../components/searchbar.js';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import '../css/discover.css';
 
-function Test() {
+const Discover = () => {
+  const [venueData, setVenueData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [records, setRecords] = useState([]); 
+  const [searchQuery, setSearchQuery] = useState('');//state variable to hold the search query.
+
+  useEffect(() => {
+    async function getVenues() {
+      const response = await fetch(`http://localhost:5050/record/`);
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const venueData = await response.json();
+      setVenueData(venueData);
+    }
+
+    getVenues();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    return;
+  }, []);
+
+  const venuesPerPage = 12;
+  const indexOfLastVenue = currentPage * venuesPerPage;
+  const indexOfFirstVenue = indexOfLastVenue - venuesPerPage;
+  const currentVenues = venueData.slice(indexOfFirstVenue, indexOfLastVenue);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= (Math.ceil(venueData.length / venuesPerPage)); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handlePageClick = (event) => {
+    setCurrentPage(Number(event.target.id));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  const renderPageNumbers = pageNumbers.map(number => (
+    <li
+      key={number}
+      id={number}
+      onClick={handlePageClick}
+    >
+      {number}
+    </li>
+  ));
+
+  useEffect(() => { 
+    async function getRecords() { // Define an function to fetch data
+      // Send a GET request to the server 
+      const response = await fetch(`http://localhost:5050/record/`);
+
+      if (!response.ok) { // Check if the response is successful
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const records = await response.json(); // Parse the response (object in database) as JSON
+
+      setRecords(records);  // Update the 'record' state with the fetched data
+    }
+
+    getRecords();  // Call fetchData function
+
+    return;
+  }, [records.length]);
+
+    // Function to update the search query
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    };
+    
   return (
-    <div style={{ padding: '20px' }}>
-      <SearchBar/>
+    <div className="discover-component" style={{marginBottom:'40px'}}>
+      <p className="section-subtitle" >Discover all nightclubs and venues in the San Marcos area </p>
+      <h2 className="h2 section-title">Discover venues</h2>
+      <div className="grid-discover-search">
+
+        <div class="item" style={{marginLeft:'10px'}}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearch}
+          placeholder="Search by name"
+          style={{borderRadius:"10px", height:"40px", background:'#fff', color:'#747474', }}
+        />
+        <button onClick={searchQuery} style={{borderRadius:"10px",  height:"40px", marginTop:'4px'}}>Find venue</button>
+
+        </div>
+
+        <div class="item">
+      <div class="container" style={{paddingTop:'40px', paddingLeft:'20px', paddingRight:'20px'}}>
+          <ul className="discover-list" >
+          {currentVenues.map((venueData) => (
+                <div className="discover-card">
+                  <figure className="card-img">
+                    <img src={venueData.image} alt={venueData.name} loading="lazy" />
+                  </figure>
+
+                  <div className="card-content">
+                    <div className="card-rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+
+                    <p className="card-subtitle">{venueData.address}</p>
+                    <h3 className="h3 card-title"><Link to={`/data/${venueData._id}`}>{venueData.name}</Link></h3>
+                    
+                    <p className="card-text">{venueData.about.length > 100 ? venueData.about.slice(0, 100) + "..." : venueData.about}</p>
+                  </div>
+                </div>
+            ))}
+          </ul>
+          <ul className="pagination">
+            {renderPageNumbers}
+        </ul>
+    </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Test;
+export default Discover;
