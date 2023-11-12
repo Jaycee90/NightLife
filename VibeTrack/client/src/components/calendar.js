a0b13cf1fff7a0b4f2801fff69790db0e0d318b4
Michelle Nguyen <rnb90@txstate.edu>
2023-11-12 16:15:39 -0600

    Stoppoint
    Not sure if react-calendar can add event

--- VibeTrack/client/src/components/calendar.js
+++ VibeTrack/client/src/components/calendar.js
@@ -16,19 +16,6 @@ function EventCalendar() {
           selectRange={true}
         />
       </div>
-      {date.length > 0 ? (
-        <p className='text-center'>
-          <span className='bold'>Start:</span>{' '}
-          {date[0].toDateString()}
-          &nbsp;|&nbsp;
-          <span className='bold'>End:</span> {date[1].toDateString()}
-        </p>
-      ) : (
-        <p className='text-center'>
-          <span className='bold'>Default selected date:</span>{' '}
-          {date.toDateString()}
-        </p>
-      )}
     </div>
   );
 };
