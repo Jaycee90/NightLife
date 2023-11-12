011eb41b79c7c17c6a3ffeb3b8c6c6a2808f36c3
Michelle Nguyen <rnb90@txstate.edu>
2023-11-12 16:39:34 -0600

    Stoppoint
    try react-event-calendar

---
+++ VibeTrack/client/src/components/eventCalendar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+//import timeGridPlugin from "@fullcalendar/timegrid";
+
+import events from "./event.js";
+
+export default function ReactCalendar() {
+  return (
+    <div className="App">
+      <FullCalendar
+        defaultView="dayGridMonth"
+        header={{
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay"
+        }}
+        themeSystem="Simplex"
+        plugins={[dayGridPlugin]}
+        events={events}
+        style={{color:'#000'}}
+      />
+    </div>
+  );
+}
--- VibeTrack/client/src/routes/Test.js
+++ VibeTrack/client/src/routes/Test.js
@@ -1,8 +1,11 @@
 
+import React from 'react';
+import ReactCalendar from '../components/eventCalendar.js';
 
 function Test(){
     return (
       <div className='test-component' style={{padding:'20px'}}>
+        <ReactCalendar/>
       </div>
     );
   };
