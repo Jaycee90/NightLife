0699ef2faf1412a14142e7118b94a2fd6a931ba2
Michelle Nguyen <rnb90@txstate.edu>
2023-11-11 21:02:25 -0600

    Added search.js back
    Missing components for some reason

--- VibeTrack/client/src/routes/Discover.js
+++ VibeTrack/client/src/routes/Discover.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../css/discover.css';
+import Fuse from 'fuse.js';
 
 const Discover = () => {
   const [venueData, setVenueData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [records, setRecords] = useState([]); 
-  const [searchQuery, setSearchQuery] = useState('');//state variable to hold the search query.
+  const [records, setRecords] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [venueFound, setVenueFound] = useState(false);
+  const [foundVenues, setFoundVenueDetails] = useState(null);
 
   useEffect(() => {
     async function getVenues() {
-      const response = await fetch(`http://localhost:5050/record/`);
+      try {
+        const response = await fetch(`http://localhost:5050/record/`);
 
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
+        if (!response.ok) {
+          throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const venueData = await response.json();
+        setVenueData(venueData);
+      } catch (error) {
+        window.alert(error.message);
       }
-
-      const venueData = await response.json();
-      setVenueData(venueData);
     }
 
     getVenues();
@@ -27,6 +32,69 @@ const Discover = () => {
 
     return;
   }, []);
+
+  useEffect(() => { 
+    async function getRecords() {
+      try {
+        const response = await fetch(`http://localhost:5050/record/`);
+
+        if (!response.ok) {
+          throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const records = await response.json();
+        setRecords(records);
+      } catch (error) {
+        window.alert(error.message);
+      }
+    }
+
+    getRecords();
+
+    return;
+  }, [records.length]);
+
+  useEffect(() => {
+    async function getRecords() {
+      try {
+        const response = await fetch(`http://localhost:5050/record/`);
+
+        if (!response.ok) {
+          throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const records = await response.json();
+        setRecords(records);
+      } catch (error) {
+        window.alert(error.message);
+      }
+    }
+
+    getRecords();
+  }, []);
+
+  const searchVenue = () => {
+    const fuse = new Fuse(records, {
+      keys: ['name'],
+      includeScore: true,
+      threshold: 0.3,
+    });
+
+    const searchResults = fuse.search(searchQuery);
+
+    if (searchResults.length > 0) {
+      const foundVenues = searchResults.map((result) => result.item);
+      setFoundVenueDetails(foundVenues);
+      setVenueFound(true);
+    } else {
+      setVenueFound(false);
+      setFoundVenueDetails(null);
+    }
+  };
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
   const venuesPerPage = 12;
   const indexOfLastVenue = currentPage * venuesPerPage;
@@ -42,7 +110,6 @@ const Discover = () => {
     setCurrentPage(Number(event.target.id));
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
   const renderPageNumbers = pageNumbers.map(number => (
     <li
       key={number}
@@ -52,54 +119,82 @@ const Discover = () => {
       {number}
     </li>
   ));
-
-  useEffect(() => { 
-    async function getRecords() { // Define an function to fetch data
-      // Send a GET request to the server 
-      const response = await fetch(`http://localhost:5050/record/`);
-
-      if (!response.ok) { // Check if the response is successful
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-
-      const records = await response.json(); // Parse the response (object in database) as JSON
-
-      setRecords(records);  // Update the 'record' state with the fetched data
-    }
-
-    getRecords();  // Call fetchData function
-
-    return;
-  }, [records.length]);
-
-    // Function to update the search query
-    const handleSearch = (event) => {
-        setSearchQuery(event.target.value);
-    };
-    
+  
   return (
     <div className="discover-component" style={{marginBottom:'40px'}}>
       <p className="section-subtitle" >Discover all nightclubs and venues in the San Marcos area </p>
       <h2 className="h2 section-title">Discover venues</h2>
       <div className="grid-discover-search">
 
-        <div class="item" style={{marginLeft:'10px'}}>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={handleSearch}
-          placeholder="Search by name"
-          style={{borderRadius:"10px", height:"40px", background:'#fff', color:'#747474', }}
-        />
-        <button onClick={searchQuery} style={{borderRadius:"10px",  height:"40px", marginTop:'4px'}}>Find venue</button>
+        <div class="item" >
+          <div class="container-search">
+          <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearch}
+              placeholder="Search by venue name"
+              style={{ borderRadius: "10px", height: "40px", fontSize:'15px', background: '#fff', color: '#747474' }}
+          />
+        <button onClick={searchVenue} style={{  backgroundColor: "#e24e99 ", borderRadius:'10px', fontSize:'15px', marginTop:'10px'}}>Find venues</button>
 
+        <hr style={{color:'#fff', marginBottom:'30px', marginTop:'20px', opacity:'0.5'}}/>
+          </div>
         </div>
 
         <div class="item">
-      <div class="container" style={{paddingTop:'40px', paddingLeft:'20px', paddingRight:'20px'}}>
-          <ul className="discover-list" >
+      <div class="container" style={{paddingTop:'20px', paddingLeft:'20px',}}>
+        
+      <div>
+          {searchQuery && (
+          <div>
+            
+      <h2 className="h2 result-title" style={{color:'#fff', fontSize:'25px', paddingBottom:'20px'}}>Search Results</h2>
+      {venueFound ? (
+        <div>
+          {foundVenues && foundVenues.length > 0 ? (
+            <div>
+              <ul className="discover-list">
+                {foundVenues.map((foundVenue) => (
+                  <div className="discover-card" key={foundVenue._id}>
+                    <figure className="card-img">
+                      <img src={foundVenue.image} alt={foundVenue.name} loading="lazy" />
+                    </figure>
+
+                    <div className="card-content">
+                      <div className="card-rating">
+                        <ion-icon name="star"></ion-icon>
+                        <ion-icon name="star"></ion-icon>
+                        <ion-icon name="star"></ion-icon>
+                        <ion-icon name="star"></ion-icon>
+                        <ion-icon name="star"></ion-icon>
+                      </div>
+
+                      <p className="card-subtitle">{foundVenue.address}</p>
+                      <h3 className="h3 card-title">
+                        <Link to={`/data/${foundVenue._id}`}>{foundVenue.name}</Link>
+                      </h3>
+
+                      <p className="card-text">
+                        {foundVenue.about.length > 80 ? foundVenue.about.slice(0, 80) + "..." : foundVenue.about}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p style={{ color: '#fff', paddingBottom: '20px' }}>{`No results found for "${searchQuery}"`}</p>
+          )}
+        </div>
+      ) : (
+        <p style={{ color: '#fff', paddingBottom: '20px', fontSize:'15px' }}>{`Searching for "${searchQuery}"...`}</p>
+      )}
+        <hr style={{color:'#fff', marginBottom:'30px', marginRight:'20px', opacity:'0.5'}}/>
+            </div>
+        )}
+      </div>
+          
+          <ul className="discover-list" style={{marginRight:'10px'}}>
           {currentVenues.map((venueData) => (
                 <div className="discover-card">
                   <figure className="card-img">
@@ -118,7 +213,7 @@ const Discover = () => {
                     <p className="card-subtitle">{venueData.address}</p>
                     <h3 className="h3 card-title"><Link to={`/data/${venueData._id}`}>{venueData.name}</Link></h3>
                     
-                    <p className="card-text">{venueData.about.length > 100 ? venueData.about.slice(0, 100) + "..." : venueData.about}</p>
+                    <p className="card-text">{venueData.about.length > 80 ? venueData.about.slice(0, 80) + "..." : venueData.about}</p>
                   </div>
                 </div>
             ))}
