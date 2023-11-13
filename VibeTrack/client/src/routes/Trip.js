c364494d4d1e686595f15abd918056a7945d6cbb
Jayce Turambe <jnn56@txstate.edu>
2023-11-13 00:56:24 -0600

    Set a view to San Marcos area

--- VibeTrack/client/src/routes/Trip.js
+++ VibeTrack/client/src/routes/Trip.js
@@ -1,42 +1,3 @@
-// import React from 'react';
-// import L from 'leaflet';
-// import 'leaflet/dist/leaflet.css';
-// import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-// import 'leaflet-routing-machine/dist/leaflet-routing-machine.js';
-
-
-
-// class TripFinder extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     // Create the map container
-//     this.mapContainer = document.createElement('div');
-//     this.mapContainer.id = 'map';
-//     document.body.appendChild(this.mapContainer);
-
-//     // Initialize the Leaflet Routing Machine library
-//     this.router = new L.Routing.OSRMv1({
-//       serviceUrl: 'https://api.osrm.io/v1'
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <p style={{ color: 'black' }}>Ready to make the dance floor jealous? Let's vibe!</p>
-//         <h2 style={{ color: 'black' }}>Discover venues near you</h2>
-//         {/* Leaflet Map Container */}
-//         <div id="map" className="leaflet-map" style={{ width: '100%', height: '500px' }}></div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default TripFinder;
-
-
-
 import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -53,7 +14,7 @@ function TripFinder() {
     // Check if the map container is already initialized
     if (!document.getElementById('map')._leaflet_id) {
       // Create the Leaflet map
-      const map = L.map('map').setView([51.505, -0.09], 13);
+      const map = L.map('map').setView([29.8833, -97.9414], 13);
 
       // Add a tile layer
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
