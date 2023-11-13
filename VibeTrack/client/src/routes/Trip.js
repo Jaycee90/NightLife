b60b4f01cd1c5ece7a58d7113c5f10301c12089b
Jayce Turambe <jnn56@txstate.edu>
2023-11-13 14:37:13 -0600

    Added leaflet compoentns abck
    Was having isuses with local branch and was unable to merge/pull request

--- VibeTrack/client/src/routes/Trip.js
+++ VibeTrack/client/src/routes/Trip.js
@@ -1,62 +1,58 @@
-import React from 'react';
-//import SimpleGMaps from 'simplegmaps-js';
+// import React, { useEffect, useState, useRef } from "react";
+import React from "react";
+//import L from "leaflet";
+import {TileLayer, MapContainer, LayersControl} from "react-leaflet";
+//import { Button } from "@material-ui/core";
+import RoutingControl from './RoutingControl'
 
-function TripFinder() {
-    return (
-        <div>
-          <p>Trip Finder!</p>
-        </div>
-    );
+const maps = {
+  base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+};
+
+const TripFinder = () => {
+  // const [map, setMap] = useState(null);
+  // const [start, setStart] = useState([29.8822, -97.9414])
+  // const [end, setEnd] = useState([30.2500, -97.7500])
+
+  return (
+    <>
+      <MapContainer
+        center={[29.8822, -97.9414]}
+        zoom={3}
+        zoomControl={false}
+        style={{ height: "71vh", width: "100%", padding: 0 }}
+        //whenCreated={map => setMap(map)}
+        whenCreated={map => {
+          // You can use map directly if needed
+          console.log("Map created:", map);
+        }}
+      >
+        {/* *************** */}
+        {/* Pass in our custom control layer here, inside of the map container */}
+        {/* *************** */}
+        <RoutingControl 
+          position={'topright'} 
+          // start={start} 
+          // end={end} 
+          start={[29.8822, -97.9414]} 
+          end={[30.2500, -97.7500]} 
+          color={'#757de8'} 
+        />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Map">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url={maps.base}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+      </MapContainer>
+    </>
+  );
 };
 
 export default TripFinder;
 
-// import React, { useEffect } from 'react';
-// import SimpleGMaps from 'simplegmaps-js';
-
-
-// function TripFinder() {
-//   useEffect(() => {
-//     // Check if SimpleGMaps is defined before using it
-//     if (SimpleGMaps) {
-//       // Initialize SimpleGMaps
-//       const myMap = new SimpleGMaps({
-//         apiKey: 'AIzaSyCB2hfKfLKS-xnrMXBH7f8wvPyvCv1rtBw',
-//       });
-
-//       myMap.init({
-//         container: '#MapContainer',
-//         GeoLocation: true,
-//         ZoomToFitBounds: true,
-//       });
-
-//       myMap.onInit = function () {
-//         // Add a marker to the map
-//         myMap.addMarker({
-//           lat: 29.890661,
-//           lng: -97.911530,
-//           title: 'San Marcos',
-//         });
-//       };
-
-//       // Cleanup SimpleGMaps when the component unmounts
-//       return () => {
-//         myMap.destroy();
-//       };
-//     } else {
-//       console.error('SimpleGMaps is not defined.');
-//     }
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Trip Finder</h1>
-//       <div id="MapContainer" style={{ height: '400px', width: '100%' }}></div>
-//     </div>
-//   );
-// }
-
-// export default TripFinder;
 
 
 
