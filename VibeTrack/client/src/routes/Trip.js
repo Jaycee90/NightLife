4bf45c37e0c13ccf5a9e900e23ab566931e0508b
Jayce Turambe <jnn56@txstate.edu>
2023-11-13 15:49:16 -0600

    Added markers on map

--- VibeTrack/client/src/routes/Trip.js
+++ VibeTrack/client/src/routes/Trip.js
@@ -1,12 +1,35 @@
 import React from "react";
-import {TileLayer, MapContainer, LayersControl} from "react-leaflet";
-import RoutingControl from './RoutingControl'
+import {TileLayer, MapContainer, LayersControl, Marker} from "react-leaflet";
+import RoutingControl from './RoutingControl';
+import L from "leaflet";
 
 const maps = {
   base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 };
 
 const TripFinder = () => {
+  const start = [29.8822, -97.9414];
+  const end = [30.2500, -97.7500];
+
+  // Custom icon for the starting point marker
+  const userMarker = new L.Icon({
+    iconUrl: 'https://i.imgur.com/wOs7nJb.png', // URL to the custom marker image
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  // Custom icon for the end point marker
+  const venueMarker = new L.Icon({
+    //iconUrl: "https://i.imgur.com/yyb78tO.png", 
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+    });
+
   return (
     <>
       <MapContainer
@@ -24,12 +47,24 @@ const TripFinder = () => {
         {/* *************** */}
         <RoutingControl 
           position={'topright'} 
-          // start={start} 
-          // end={end} 
-          start={[29.8822, -97.9414]} 
-          end={[30.2500, -97.7500]} 
+          start={start} 
+          end={end} 
+          // start={[29.8822, -97.9414]} 
+          // end={[30.2500, -97.7500]} 
           color={'#757de8'} 
         />
+        
+        {/* Add Marker for Start Position */}
+        <Marker position={start} icon={userMarker} draggable={true}>
+          {/* You can customize the marker icon here */}
+        </Marker>
+
+        {/* Add Marker for End Position */}
+        <Marker position={end} icon={venueMarker} draggable={true}>
+          {/* You can customize the marker icon here */}
+        </Marker>
+
+
         <LayersControl position="topright">
           <LayersControl.BaseLayer checked name="Map">
             <TileLayer
