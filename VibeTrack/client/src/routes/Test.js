b49b99629dba5c40941ea2a205194ec59bfd8106
Michelle Nguyen <rnb90@txstate.edu>
2023-11-15 21:36:40 -0600

    Tags working with venue database
    Need to refine and patch up to Discover

--- VibeTrack/client/src/routes/Test.js
+++ VibeTrack/client/src/routes/Test.js
@@ -1,90 +1,46 @@
-import { useState, useCallback, useId } from 'react';
+import { useState, useCallback, useEffect, useId } from 'react';
 import '../css/test.css';
 
 function Test() {
+  // State to manage selected tags
   const [tags, setTags] = useState([]);
 
+  // Generate a unique ID for elements with the useId hook
   const id = useId();
 
-  const projects = [
-    {
-      title: 'Example 0',
-      description:
-        'This is an example project item. You can sort through these using the tags. You can also click a tag to add it to the filter.',
-      tags: ['react', 'javascript', 'game', 'back-end'],
-    },
-    {
-      title: 'Game of Life',
-      description: 'React implementation of the game of life.',
-      tags: ['react', 'javascript', 'game', 'web', 'front-end'],
-    },
-    {
-      title: 'Calculator',
-      description: 'Calculator written in Javascript',
-      tags: ['javascript', 'utility', 'web'],
-    },
-    {
-      title: 'Tic Tac Toe',
-      description: 'A command-line Tic Tac Toe game written in Ruby',
-      tags: ['ruby', 'game', 'cli'],
-    },
-    {
-      title: 'TodoList',
-      description: 'Full stack todo-list written in fullstack Javascript',
-      tags: [
-        'react',
-        'javascript',
-        'node',
-        'fullstack',
-        'front-end',
-        'back-end',
-        'web',
-        'mvc',
-      ],
-    },
-    {
-      title: 'Weather',
-      description: 'A Weather App with React Native ',
-      tags: [
-        'react',
-        'javascript',
-        'react-native',
-        'front-end',
-        'mobile',
-        'android',
-        'ios',
-      ],
-    },
-    {
-      title: 'Markdown Editor',
-      description: 'Markdown Editor powered by Monaco and React',
-      tags: ['react', 'javascript', 'monaco', 'front-end'],
-    },
-    {
-      title: 'Bloggie',
-      description: 'Rails-powered blog with a React front-end',
-      tags: [
-        'react',
-        'javascript',
-        'ruby',
-        'front-end',
-        'back-end',
-        'fullstack',
-        'ruby-on-rails',
-        'mvc',
-      ],
-    },
-  ];
+  // State to store venue data
+  const [venueData, setVenueData] = useState([]);
 
+  useEffect(() => {
+    async function getRecords() {
+      try {
+        const response = await fetch(`http://localhost:5050/record/`);
+
+        if (!response.ok) {
+          throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const records = await response.json();
+        setVenueData(records);
+      } catch (error) {
+        window.alert(error.message);
+      }
+    }
+
+    getRecords();
+  }, []);
+
+  // Function to add a tag to the selected tags state
   const addTag = useCallback(
     (tag) => () => {
       if (!tags.includes(tag)) {
-        return setTags((prevTags) => [...prevTags, tag]);
+        setTags((prevTags) => [...prevTags, tag]);
       }
     },
     [tags]
   );
 
+  // Function to remove a tag from the selected tags state
   const deleteTag = useCallback(
     (tagId) => () => {
       const tagsFiltered = tags.filter((tag) => tag !== tagId);
@@ -93,25 +49,27 @@ function Test() {
     [tags]
   );
 
+  // Function to check if a venue matches the selected tags
   const matchTags = (current, target) => {
     return target.every((tag) => current.includes(tag));
   };
 
-  // Extract unique tags
-  const uniqueTags = Array.from(new Set(projects.flatMap((proj) => proj.tags)));
+  // Extract unique tags from all venues
+  const uniqueTags = Array.from(new Set(venueData.flatMap((venue) => venue.tags)));
 
   return (
     <div className="test-component" style={{ padding: '100px' }}>
       <div className='tags-container'>
         <h1 className='tag-filter'>Tags filtered</h1>
         <div>
+          {/* Display selected tags */}
           {tags.length > 0
             ? tags.map((tag) => (
                 <button
                   key={`close-button-${id}-${tag}`}
                   className='close'
                   onClick={deleteTag(tag)}
-                  style={{width:'120px', marginBottom:'10px'}}
+                  style={{ width: '120px', marginBottom: '10px' }}
                 >
                   {tag} &nbsp; x
                 </button>
@@ -120,27 +78,28 @@ function Test() {
         </div>
       </div>
 
-      {/* Display unique tags */}
+      {/* Display unique tags as buttons */}
       <div className='unique-tags'>
         {uniqueTags.map((tag) => (
           <button
             key={`unique-tag-${id}-${tag}`}
             type='button'
             onClick={addTag(tag)}
-            style={{width:'90px'}}
+            style={{ width: '90px' }}
           >
             #{tag}
           </button>
         ))}
       </div>
 
-      {/* Display projects without tags */}
-      {projects
-        .filter((proj) => matchTags(proj.tags, tags))
-        .map(({ title, description }) => (
+      {/* Display venues without tags */}
+      {venueData
+        .filter((venue) => matchTags(venue.tags, tags))
+        .map(({ name, description }) => (
           <div key={`card-${id}`} className='card'>
             <div>
-              <p>{title}</p>
+              {/* Display venue information */}
+              <p>{name}</p>
               <p>{description}</p>
             </div>
           </div>
