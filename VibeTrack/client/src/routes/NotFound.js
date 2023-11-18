c81bf32ee67380b186e0b227264c16c9c0175216
Benu Liburd <bjl98@txstate.edu>
2023-11-17 23:14:11 -0600

    added function
    added code to the NotFound function

--- VibeTrack/client/src/routes/NotFound.js
+++ VibeTrack/client/src/routes/NotFound.js
@@ -1,1 +1,20 @@
 //initial commit
+
+// NotFound.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h2>404 Not Found</h2>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/home">
+        <button style={{ padding: '10px', marginTop: '10px' }}>Go to Home</button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
