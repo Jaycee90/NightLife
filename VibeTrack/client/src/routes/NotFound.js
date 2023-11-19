947dff7952c2c92f68cb1c619b2d6e7dec24133f
Benu Liburd <bjl98@txstate.edu>
2023-11-18 21:31:10 -0600

    testing visuals

--- VibeTrack/client/src/routes/NotFound.js
+++ VibeTrack/client/src/routes/NotFound.js
@@ -1,15 +1,14 @@
-//initial commit
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/NotFound.css';
 
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h2>404 Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link to="/home">
-        <button style={{ padding: '10px', marginTop: '10px' }}> Go to Home</button>
+    <div className="not-found-container">
+      <h2 className="not-found-title">404 Not Found</h2>
+      <p className="not-found-message">Sorry, the page you are looking for does not exist.</p>
+      <Link to="/">
+        <button className="not-found-button">Go to Home</button>
       </Link>
     </div>
   );
