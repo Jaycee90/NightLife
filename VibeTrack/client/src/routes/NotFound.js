688b0c4a2b3be287f2ab2569493364998c7bea63
Benu Liburd <bjl98@txstate.edu>
2023-11-18 21:16:26 -0600

    created NotFound.css
    created a .css file to make the NotFound.js code cleaner

--- VibeTrack/client/src/routes/NotFound.js
+++ VibeTrack/client/src/routes/NotFound.js
@@ -9,7 +9,7 @@ const NotFound = () => {
       <h2>404 Not Found</h2>
       <p>Sorry, the page you are looking for does not exist.</p>
       <Link to="/home">
-        <button style={{ padding: '10px', marginTop: '10px' }}>Go to Home</button>
+        <button style={{ padding: '10px', marginTop: '10px' }}> Go to Home</button>
       </Link>
     </div>
   );
