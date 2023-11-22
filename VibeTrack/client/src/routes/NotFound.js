c976a995d39d94690b65186fce1b06bf118f9c1b
Michelle Nguyen <rnb90@txstate.edu>
2023-11-21 19:44:34 -0600

    Stoppoint: Basic customizing 404

--- VibeTrack/client/src/routes/NotFound.js
+++ VibeTrack/client/src/routes/NotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/NotFound.css';
+import '../css/notfound.css';
 
 const NotFound = () => {
   return (
     <div className="not-found-container">
       <h2 className="not-found-title">404 Not Found</h2>
-      <p className="not-found-message">Sorry, the page you are looking for does not exist.</p>
+      <p className="not-found-message">Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.</p>
       <Link to="/">
-        <button className="not-found-button">Go to Home</button>
+        <button className="not-found-button">Return to Homepage</button>
       </Link>
     </div>
   );
