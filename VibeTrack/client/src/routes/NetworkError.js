46250feec99730c595bfe31e8705d98db2079282
Benu Liburd <bjl98@txstate.edu>
2023-11-25 16:52:14 -0600

    created a .css file and made changes to NetworkError.js

--- VibeTrack/client/src/routes/NetworkError.js
+++ VibeTrack/client/src/routes/NetworkError.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/NotFound.css';
+import '../css/NetworkError.css'; 
 
 const NetworkError = () => {
   return (
-    <div className="not-found-container">
-      <h2 className="not-found-title">404 Not Found</h2>
-      <p className="not-found-message">Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.</p>
+    <div className="network-error-container">
+      <h2 className="network-error-title">Network Error</h2>
+      <p className="network-error-message">Oops! It seems there was a problem connecting to the network.</p>
       <Link to="/">
-        <button className="not-found-button">Return to Homepage</button>
+        <button className="network-error-button">Return to Homepage</button>
       </Link>
     </div>
   );
 };
 
 export default NetworkError;
+
