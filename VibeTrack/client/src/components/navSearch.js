b443142c72ae6b46e531b03e76266e4a0d186b26
Michelle Nguyen <rnb90@txstate.edu>
2023-11-25 09:53:01 -0600

    Stoppoint: can navigate to most matched venues

--- VibeTrack/client/src/components/navSearch.js
+++ VibeTrack/client/src/components/navSearch.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Fuse from 'fuse.js';
+import { FaSearch } from "react-icons/fa";
 
 const NavSearch = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [foundVenueDetails, setFoundVenueDetails] = useState(null);
   // eslint-disable-next-line
   const [venueData, setVenueData] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getVenues() {
@@ -37,64 +38,54 @@ const NavSearch = () => {
   const searchVenue = async () => {
     try {
       const response = await fetch(`http://localhost:5050/record/`);
-  
+
       if (!response.ok) {
         throw new Error(`An error occurred: ${response.statusText}`);
       }
-  
+
       const records = await response.json();
-  
+
       const fuse = new Fuse(records, {
         keys: ['name'],
         includeScore: true,
         threshold: 0.3,
       });
-  
+
       const searchResults = fuse.search(searchQuery);
-  
+
       if (searchResults.length > 0) {
         // Find the result with the lowest score (most matched)
         const mostMatchedVenue = searchResults.reduce((prev, current) => {
           return prev.score < current.score ? prev : current;
         }).item;
-  
-        setFoundVenueDetails([mostMatchedVenue]);
+
+        // Navigate to the page
+        navigate(`/data/${mostMatchedVenue._id}`);
       } else {
-        setFoundVenueDetails(null);
+        // Handle the case where no results are found
+        window.alert('No results found.');
       }
     } catch (error) {
       window.alert(error.message);
     }
   };
-  
-
-  // This useEffect will trigger the search when the searchQuery changes
-  useEffect(() => {
-    searchVenue();
-    // eslint-disable-next-line
-  }, [searchQuery]);
 
   return (
     <div>
       {/* Add a form to input search query */}
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => { e.preventDefault(); searchVenue(); }}>
         <input
           type="text"
           placeholder="Search for venues..."
           value={searchQuery}
           onChange={handleSearch}
-            style={{ backgroundColor:"#fff" , color:'#747474', paddingBottom:'10px', marginTop:'0px', borderRadius:'10px'}}
-            inputProps={{ style: { backgroundColor: "#fff", color:'#747474'} }}
+          style={{ backgroundColor:"#fff" , color:'#747474', paddingBottom:'10px', marginTop:'0px', borderRadius:'10px'}}
+          inputProps={{ style: { backgroundColor: "#fff", color:'#747474'} }}
         />
+        <button type="submit" onClick={searchVenue}>
+          <FaSearch style={{color:'#fff'}}/>
+        </button>
       </form>
-        <div>
-      {/* Display search results */}
-      {foundVenueDetails &&
-        foundVenueDetails.map((venueData) => (
-          <div key={venueData._id}>
-            <Link to={`/data/${venueData._id}`}>{venueData.name}</Link>
-          </div>
-        ))}</div>
     </div>
   );
 };
