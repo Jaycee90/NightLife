615650d9fd7e5f228ad32718b071fe2e8fd6bcce
Michelle Nguyen <rnb90@txstate.edu>
2023-11-27 00:13:10 -0600

    Customized Home again
    Trip Finder need fuzzy search

--- VibeTrack/client/src/routes/Home.js
+++ VibeTrack/client/src/routes/Home.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
+import { FaStar } from "react-icons/fa";
 import '../css/home.css';
+import { Link } from "react-router-dom";
+
 export default function Home() {
   const [randomVenues, setRandomVenues] = useState([]);
   const navigate = useNavigate();
@@ -38,9 +40,10 @@ export default function Home() {
     return randomVenues;
   };
 
+  const stars = Array(5).fill(0);   
   return (
     <div  style={{marginTop:"20px"}}>
-      <div className="section-intro">
+      <div className="section-intro" style={{backgroundImage:'url(https://i.imgur.com/bQfI5Uz.png)', backgroundSize:'cover'}}>
         <img src="https://i.imgur.com/SoHE2tO.png" alt="Logo" loading="lazy" style={{ height:"125px"}} />
         <p className="section-subtitle" style={{color:'#fff'}}>Uncover places, discover world</p>
         <h2 className="h2 section-title" style={{color:'#fff'}}>VibeTrack</h2>
@@ -59,22 +62,22 @@ export default function Home() {
           <h2 className="h2 section-title">What we do</h2>
           <div className="about-project">
             <div className="item">
-              <img src="https://i.imgur.com/Pqg9FDn.png" className="circle" alt="Placeholder" loading="lazy"/>
+              <img src="https://i.imgur.com/al6ByNd.png" className="circle" alt="Placeholder" loading="lazy"/>
               <hr className="hr-style"/>
-              <h3  style={{marginTop:'20px'}}>Find Events</h3>
-              <p className="section-text">Wondering what’s going on this weekend? VibeTrack curates a list of events happening in the San Marcos area.</p>
+              <h3  style={{marginTop:'20px'}}>Search for Events</h3>
+              <p className="section-text">Wondering what’s going on this weekend? We consistently curates a list of upcoming events in the San Marcos area.</p>
             </div>
             <div className="item">
-              <img src="https://i.imgur.com/Pqg9FDn.png" className="circle" alt="Placeholder" loading="lazy"/>
+              <img src="https://i.imgur.com/Em15dzc.jpg" className="circle" alt="Placeholder" loading="lazy"/>
               <hr className="hr-style"/>
-              <h3  style={{marginTop:'20px'}}>Find Events</h3>
-              <p className="section-text">Wondering what’s going on this weekend? Discotech curates a list of events happening in the San Marcos area.</p>  
+              <h3  style={{marginTop:'20px'}}>Club Finder</h3>
+              <p className="section-text">Not sure where to hang? Discover clubs nearby that cater to your preferences and make the shortest trip to get there! </p>  
             </div>
             <div className="item">
-              <img src="https://i.imgur.com/Pqg9FDn.png"  className="circle" alt="Placeholder" loading="lazy"/>
+              <img src="https://i.imgur.com/6qmK0E2.png"  className="circle" alt="Placeholder" loading="lazy"/>
               <hr className="hr-style"/>
-              <h3  style={{marginTop:'20px'}}>Find Events</h3>
-              <p className="section-text">Wondering what’s going on this weekend? Discotech curates a list of events happening in the San Marcos area.</p>
+              <h3  style={{marginTop:'20px'}}>Alert & Invite </h3>
+              <p className="section-text">Your safety is our top priority! Let friends, colleagues and family know where you're heading tonight with simple tap.</p>
               </div>
           </div>
       </div>
@@ -83,8 +86,7 @@ export default function Home() {
       <section className="popular" id="destination">
         <div className="container">
           <h2 className="h2 section-title">Discover venues</h2>
-          <p className="section-subtitle" >Experience nightlife in San Marcos, TX. Enjoy live music, late-night restaurants, bars, and dog-friendly outdoor decks. There's always something going on around you! </p>
-
+          <p className="section-text" style={{textAlign:'center'}} >Experience nightlife in San Marcos, TX. Enjoy live music, late-night restaurants, bars, and dog-friendly outdoor decks. There's always something going on around you! </p>
 
           <ul className="popular-list" >
             {randomVenues.map((venue, index) => (
@@ -94,27 +96,88 @@ export default function Home() {
                     <img src={venue.image} alt={venue.name} loading="lazy" />
                   </figure>
 
-                  <div className="card-content">
-                    <div className="card-rating">
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
+                  <div className="card-content" >
+                  <div className="card-rating">
+                        <p style={{color:'#747474'}}>{venue.price}</p>
+                      {stars.map((value, index) => {
+                        const decimalPart = venue.rating - Math.floor(venue.rating);
+                        const roundUpThreshold = 0.5;
+                        const roundedRating = decimalPart > roundUpThreshold ? Math.ceil(venue.rating) : Math.floor(venue.rating);
+
+                        return (
+                          <FaStar
+                            key={index}
+                            size={15}
+                            color={index < roundedRating ? '#FFBA5A' : '#a9a9a9'}
+                          />
+                        );
+                      })}
                     </div>
 
                     <p className="card-subtitle">{venue.address}</p>
-                    <h3 className="h3 card-title"><a href={venue.website}>{venue.name}</a></h3>
+                    <h3 className="h3 card-title"><Link to={`/data/${venue._id}`}>{venue.name}</Link></h3>
 
                     <p className="card-text">{venue.description}</p>
                   </div>
                 </div>
               </li>
             ))}
-          </ul>
-          <button className="btn btn-primary" style={{width:'50%',backgroundColor: "#e24e99 ", color:'#fff', borderRadius:"10px", marginBottom:'20px', }}>Discover even more venues near you</button>
-        </div>
+          </ul></div>
       </section>
+      <hr className="hr-style"/>
+      <div className="container">
+          
+          <div className="about-something">
+            <div class="item">
+            <h2 className="h2 section-title">Find the venue that is right for you </h2>
+              <ul className="section-text" style={{textAlign:'left', paddingLeft:'30px', paddingRight:'30px',  fontFamily:'Segoe UI'}}>
+                <li style={{ marginBottom: '10px' }}>✦ Discover nearby clubs that cater to your preferences  </li>
+                <li style={{ marginBottom: '10px' }}>✦ Filter by venue type, features and price range</li>
+                <li style={{ marginBottom: '10px' }}>✦ Invite your friends to come and join the fun</li>
+              </ul>
+              </div>
+            
+            <div class="item">
+            <img src="https://i.imgur.com/L04j96J.png" alt="Discover" loading="lazy" style={{width:'50vw'}}/>
+          </div>
+          </div>
+      </div>
+      <hr className="hr-style"/>
+      <div className="container">
+          
+          <div className="about-something">
+            <div class="item">
+              <img src="https://i.imgur.com/avz8PKE.png" alt="Special Event" loading="lazy" style={{width:'50vw'}}/>
+            </div>
+            <div class="item">
+            <h2 className="h2 section-title">Search for Special Event</h2>
+              <ul className="section-text" style={{textAlign:'left', paddingLeft:'30px', paddingRight:'50px',  fontFamily:'Segoe UI'}}>
+                <li style={{ marginBottom: '10px' }}>✦ Consisttently update upcoming special events such as live music, standup comedy, etc.  </li>
+                <li style={{ marginBottom: '10px' }}>✦ Checkout what's happening around San Marcos this weekend</li>
+                <li style={{ marginBottom: '10px' }}>✦ Set it off and make it a night to remember</li>
+              </ul>
+              </div>
+          </div>
+      </div>
+
+      <hr className="hr-style"/>
+      <div className="container">
+          
+          <div className="about-something">
+            <div class="item">
+            <h2 className="h2 section-title">Hit the Road with TripFinder </h2>
+              <ul className="section-text" style={{textAlign:'left', paddingLeft:'30px', paddingRight:'30px',  fontFamily:'Segoe UI'}}>
+                <li style={{ marginBottom: '10px' }}>✦ Plan your party schedule ahead of time  </li>
+                <li style={{ marginBottom: '10px' }}>✦ Search for clubs and venues that are closest to you</li>
+                <li style={{ marginBottom: '10px' }}>✦ Find the shortest trip to your destination</li>
+              </ul>
+              </div>
+            
+            <div class="item">
+            <img src="https://i.imgur.com/fcSjozN.png" alt="TripFinder" loading="lazy" style={{width:'50vw'}}/>
+          </div>
+          </div>
+      </div>
     </div>
   );
 }
