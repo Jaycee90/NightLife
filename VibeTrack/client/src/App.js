064545de1a279649a04e633e81cde2bb92588875
Michelle Nguyen <rnb90@txstate.edu>
2023-11-27 16:01:34 -0600

    Fixed footer/disclaimer for App

--- VibeTrack/client/src/App.js
+++ VibeTrack/client/src/App.js
@@ -19,6 +19,7 @@ import RecordList from "./components/recordList";
 import UserList from "./components/userList";
 import StarRating from "./components/starRating";
 import Footer from './components/footer';
+import Disclaimer from "./components/disclaimer";
 
 import Data from './routes/Data'; 
 import Discover from './routes/Discover';
@@ -40,38 +41,37 @@ function App() {
      <UserProvider>
      <Navbar />
        <Routes>
-       <Route exact path="/" element={<Home />} />
-            <Route exact path='/discover' element={<Discover />} />
-            <Route exact path="/data/:id" element={<Data />} />
-            <Route exact path='/specialevent' element={<SpecialEvent/>} />
-            <Route exact path='/feature' element={<Feature/>} />
-            <Route exact path='/search' element={<Search />} />
-            <Route exact path='/tripfinder' element={<TripFinder />} /> 
-            <Route exact path='/tripfinder/:venueName' element={<TripFinder />} /> 
-            <Route exact path='/login' element={<Login />} />
-            <Route exact path='/safety' element={<Safety />} />
-            <Route exact path='/recordlist' element={<RecordList />} />
-            <Route exact path="/edit/:id" element={<Edit />} />         
-            <Route exact path="/create" element={<Create />} />
-            <Route exact path='/invitation' element={<Invitation />} />
-            <Route exact path='/rating/:id' element={<StarRating />} />  
-            <Route exact path='*' element={<NotFound />} />    
-            <Route exact path='/error' element={<NetworkError />} />  
-            <Route exact path='/feedback' element={<Feedback />} />
-            <Route exact path='/test' element={<Test />} /> 
+       <Route exact path="/" element={<><Home /> <Footer/></>} />
+            <Route exact path='/discover' element={<><Discover /> <Disclaimer/></>} />
+            <Route exact path="/data/:id" element={<><Data /> <Disclaimer/></>} />
+            <Route exact path='/specialevent' element={<><SpecialEvent/>  <Disclaimer/></>} />
+            <Route exact path='/feature' element={<><Feature/>  <Disclaimer/></>} />
+            <Route exact path='/search' element={<><Search />  <Disclaimer/></>} />
+            <Route exact path='/tripfinder' element={<><TripFinder />  <Disclaimer/></>} /> 
+            <Route exact path='/tripfinder/:venueName' element={<><TripFinder />  <Disclaimer/></>} /> 
+            <Route exact path='/login' element={<><Login />  <Disclaimer/></>} />
+            <Route exact path='/safety' element={<><Safety />  <Disclaimer/></>} />
+            <Route exact path='/recordlist' element={<><RecordList />  <Disclaimer/></>} />
+            <Route exact path="/edit/:id" element={<><Edit />  <Disclaimer/></>} />         
+            <Route exact path="/create" element={<><Create />  <Disclaimer/></>} />
+            <Route exact path='/invitation' element={<><Invitation />  <Disclaimer/></>} />
+            <Route exact path='/rating/:id' element={<><StarRating />  <Disclaimer/></>} />  
+            <Route exact path='*' element={<><NotFound />  <Disclaimer/></>} />    
+            <Route exact path='/error' element={<><NetworkError />  <Disclaimer/></>} />  
+            <Route exact path='/feedback' element={<><Feedback />  <Disclaimer/></>} />
+            <Route exact path='/test' element={<><Test />  <Disclaimer/></>} /> 
 
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route element={<PrivateRoute />}>
-                <Route exact path='/profile/' element={<Profile />} />
-                <Route exact path='/contact/' element={<Contact />} />
-                <Route exact path='/userList' element={<UserList />} />
-                <Route exact path='/favorite' element={<Favorite />} />
+            <Route exact path="/signup" element={<><Signup />  <Disclaimer/></>} />
+            <Route exact path="/login" element={<><Login />  <Disclaimer/></>} />
+            <Route element={<><PrivateRoute />  <Disclaimer/></>}>
+                <Route exact path='/profile/' element={<><Profile />  <Disclaimer/></>} />
+                <Route exact path='/contact/' element={<><Contact />  <Disclaimer/></>} />
+                <Route exact path='/userList' element={<><UserList />  <Disclaimer/></>} />
+                <Route exact path='/favorite' element={<><Favorite />  <Disclaimer/></>} />
             </Route>
-            <Route exact path='/security' element={<Security />} />
-            <Route exact path='/reset' element={<Reset />} />
+            <Route exact path='/security' element={<><Security />  <Disclaimer/></>} />
+            <Route exact path='/reset' element={<><Reset />  <Disclaimer/></>} />
        </Routes>
-      <Footer />
      </UserProvider>
    </BrowserRouter>
  );
