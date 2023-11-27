5034a8f1e9baa7133191d35db0d981961e6bac03
Michelle Nguyen <rnb90@txstate.edu>
2023-11-26 22:40:33 -0600

    Customized 400 and 500 error page

--- VibeTrack/client/src/routes/NetworkError.js
+++ VibeTrack/client/src/routes/NetworkError.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/NetworkError.css'; 
+import '../css/notfound.css'; 
 
 const NetworkError = () => {
   return (
-    <div className="network-error-container">
-      <h2 className="network-error-title">Network Error</h2>
-      <p className="network-error-message">Oops! It seems there was a problem connecting to the network.</p>
+    <div  style={{backgroundImage:'url(https://i.imgur.com/VO79Out.png)',  backgroundPosition: 'right', backgroundSize: 'cover', height: '60vh'}}>
+    <div className="not-found-container" style={{margin:'20vh 10vw'}}>
+      <h2 className="not-found-title">Network Error</h2>
+      <p className="not-found-message">Oops! It seems there was a problem connecting to the network, try reloading the page.</p>
       <Link to="/">
-        <button className="network-error-button">Return to Homepage</button>
+        <button className="not-found-button">Return to Homepage</button>
       </Link>
+    </div>
     </div>
   );
 };
