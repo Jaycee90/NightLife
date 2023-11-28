1b95c9c2d699afefb6e9b47886efd70a9c7a9641
Michelle Nguyen <rnb90@txstate.edu>
2023-11-28 01:27:34 -0600

    Attempt again at removing notfound.css
    ??? i'm so lost

--- VibeTrack/client/src/routes/NetworkError.js
+++ VibeTrack/client/src/routes/NetworkError.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import './notfound.css'; 
 import { Link } from 'react-router-dom';
 
 const NetworkError = () => {
   return (
     <div  style={{backgroundImage:'url(https://i.imgur.com/VO79Out.png)',  backgroundPosition: 'right', backgroundSize: 'cover', height: '60vh'}}>
-    <div className="not-found-container" style={{margin:'20vh 10vw'}}>
-      <h2 className="not-found-title">Network Error</h2>
-      <p className="not-found-message">Oops! It seems there was a problem connecting to the network, try reloading the page.</p>
+    <div className="not-found-container" style={{margin: '20vh 60vw', textAlign: 'center', padding: '40px', borderRadius: '8px', maxWidth: '500px', width: '100%', fontFamily: 'Segoe UI'}}>
+      <h2 className="not-found-title" style={{fontSize: '2em', color: '#747474', fontFamily: 'Segoe UI', padding: '20px'}}>Network Error</h2>
+      <p className="not-found-message" style={{ fontFamily: 'Segoe UI', color: '#747474', padding: '20px' }}>Oops! It seems there was a problem connecting to the network, try reloading the page.</p>
       <Link to="/">
-        <button className="not-found-button">Return to Homepage</button>
+        <button className="not-found-button" style={{ fontFamily: 'Segoe UI', padding: '10px', marginTop: '10px', backgroundColor: '#e24e99', color: '#fff' }}>Return to Homepage</button>
       </Link>
     </div>
     </div>
--- VibeTrack/client/src/routes/NotFound.js
+++ VibeTrack/client/src/routes/NotFound.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import './notfound.css'; 
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div  style={{backgroundImage:'url(https://i.imgur.com/A6SE0q4.png)',  backgroundPosition: 'left', backgroundSize: 'cover', height: '60vh'}}>
-      <div className="not-found-container" style={{margin: '20vh 60vw'}}>
-      <h2 className="not-found-title">404 Not Found</h2>
-      <p className="not-found-message">Oops! Looks like you followed a bad link. If you think this is a problem with us, please let us know.</p>
+      <div className="not-found-container" style={{margin: '20vh 60vw', textAlign: 'center', padding: '40px', borderRadius: '8px', maxWidth: '500px', width: '100%', fontFamily: 'Segoe UI'}}>
+      <h2 className="not-found-title" style={{fontSize: '2em', color: '#747474', fontFamily: 'Segoe UI', padding: '20px'}}>404 Not Found</h2>
+      <p className="not-found-message" style={{ fontFamily: 'Segoe UI', color: '#747474', padding: '20px' }}>Oops! Looks like you followed a bad link. If you think this is a problem with us, please let us know.</p>
       <Link to="/">
-        <button className="not-found-button">Return to Homepage</button>
+        <button className="not-found-button" style={{ fontFamily: 'Segoe UI', padding: '10px', marginTop: '10px', backgroundColor: '#e24e99', color: '#fff' }}>Return to Homepage</button>
       </Link>
     </div>
     </div>
