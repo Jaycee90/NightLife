3379a8a17f4ba37df51066fc59b0ecb62537ab83
Michelle Nguyen <rnb90@txstate.edu>
2023-11-28 01:44:10 -0600

    Last attempt: deployed but typos in NetworkEerror

--- VibeTrack/client/src/routes/NetworkError.js
+++ VibeTrack/client/src/routes/NetworkError.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NetworkError = () => {
   return (
     <div  style={{backgroundImage:'url(https://i.imgur.com/VO79Out.png)',  backgroundPosition: 'right', backgroundSize: 'cover', height: '60vh'}}>
-    <div className="not-found-container" style={{margin: '20vh 60vw', textAlign: 'center', padding: '40px', borderRadius: '8px', maxWidth: '500px', width: '100%', fontFamily: 'Segoe UI'}}>
+    <div className="not-found-container" style={{margin: '20vh 10vw', textAlign: 'center', padding: '40px', borderRadius: '8px', maxWidth: '500px', width: '100%', fontFamily: 'Segoe UI'}}>
       <h2 className="not-found-title" style={{fontSize: '2em', color: '#747474', fontFamily: 'Segoe UI', padding: '20px'}}>Network Error</h2>
       <p className="not-found-message" style={{ fontFamily: 'Segoe UI', color: '#747474', padding: '20px' }}>Oops! It seems there was a problem connecting to the network, try reloading the page.</p>
       <Link to="/">
